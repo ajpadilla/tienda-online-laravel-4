77fdea07315d4e628a04c4a44c4792898992805e
Lenyn Alcantara <nightzpy@gmail.com>
2014-11-05 08:44:54 +0000

    Estructura basica para registro de productos

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProductController extends \BaseController {
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+}
