e1937538c2435b1f2f19a98f97438ffa07b5b982
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-06 10:42:42 -0430

    Agregando modelo y repository discounts al proyecto

---
+++ app/s4h/store/Discounts/Discount.php
@@ -0,0 +1,7 @@
+<?php namespace s4h\store\Discounts;
+
+use Eloquent;
+
+class Discount extends Eloquent {
+	protected $fillable = ['name','description','value','percent','quantity','quantity_per_user','code','active','from','to','discount_type_id'];
+}
