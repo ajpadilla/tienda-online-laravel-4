9dbbfb067b5a331d8a589939b5b0c09b21974a31
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-06 11:12:10 -0430

    Agregando vistas para los datos del modelo Discount

---
+++ app/views/discounts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.template')
+
+@section('title')
+    {{--{{ Lang::get('modulo.variable') }}--}}
+@stop
+
+@section('content')
+	<h1>Discounts</h1>
+@stop
+
+@section('scripts')
+  <script>
+    $(document).ready(function(){
+
+    });
+  </script>
+@stop
