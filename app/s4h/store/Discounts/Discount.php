dcc54080b05a48c3a0cd94af8a73d88985ed309b
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-06 15:53:33 -0430

    Agregando clases para el modelo discounts

--- app/s4h/store/Discounts/Discount.php
+++ app/s4h/store/Discounts/Discount.php
@@ -3,5 +3,8 @@
 use Eloquent;
 
 class Discount extends Eloquent {
+	
+	protected $table = 'discounts';
+
 	protected $fillable = ['name','description','value','percent','quantity','quantity_per_user','code','active','from','to','discount_type_id'];
 }
