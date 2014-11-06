63059f7ee1ae2857c735e4cef837ec26deb4b071
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-06 15:56:02 -0430

    Actualizando datos

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb5523cae3ac9c7d8bf1986d09f8da2ef::getLoader();
+return ComposerAutoloaderInit4719b9427dd572e76dc14e80584eb367::getLoader();
