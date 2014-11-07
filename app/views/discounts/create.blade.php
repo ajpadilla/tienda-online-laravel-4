b9769f439688a32da74254d6ba2235dabaacb311
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-06 22:47:15 -0430

    Agregando vista para cargar los datos del modelo Discount_type

--- app/views/discounts/create.blade.php
+++ app/views/discounts/create.blade.php
@@ -21,7 +21,7 @@
 				@endif
 			</div>
 			<div class="ibox-content">
-				{{ Form::open(['route' => 'discounts.store','class'=>'form-horizontal','method' => 'POST']) }}
+				{{ Form::open(['route' => 'discounts.store','class'=>'form-horizontal','method' => 'POST','id' => 'formCreateDiscount']) }}
 				<div class="form-group">
 					{{ Form::label('name', 'Name:',['class'=>'col-sm-2 control-label']) }}
 					<div class="col-sm-8">
@@ -60,6 +60,12 @@
 					{{ Form::label('quantity_per_user', 'Quantity Per User:',['class'=>'col-sm-2 control-label']) }}
 					<div class="col-sm-8">
 						{{ Form::text('quantity_per_user',null, ['class' => 'form-control']) }}
+					</div>
+				</div>
+				<div class="form-group">
+					{{ Form::label('code', 'Code:',['class'=>'col-sm-2 control-label']) }}
+					<div class="col-sm-8">
+						{{ Form::text('code',null, ['class' => 'form-control']) }}
 					</div>
 				</div>
 				<div class="hr-line-dashed"></div>
@@ -117,9 +123,53 @@
 			radioClass: 'iradio_square-green',
 		});
 
-		$('#from').datepicker();
-		$('#to').datepicker();
+		$('#from').datepicker({
+			showButtonPanel: true,
+			changeMonth: true,
+			changeYear: true,
+			yearRange: '2014:2020',
+			dateFormat: 'yy-mm-dd'
+		});
 
+		$('#to').datepicker({
+			showButtonPanel: true,
+			changeMonth: true,
+			changeYear: true,
+			yearRange: '2002:2012', 
+			dateFormat: 'yy-mm-dd'
+		});
+
+		var options = { 
+				beforeSubmit:  showRequest,  // pre-submit callback 
+				success:       showResponse,  // post-submit callback 
+				url:  '{{URL::route('discounts.store')}}',
+        		type:'POST'
+			};
+		$('#formCreateDiscount').ajaxForm(options);
 	});
+
+	// pre-submit callback 
+		function showRequest(formData, jqForm, options) {          
+			setTimeout(jQuery.fancybox({
+				'content': '<h1>Enviando datos</h1>',
+				'autoScale' : true,
+				'transitionIn' : 'none',
+				'transitionOut' : 'none',
+				'scrolling' : 'no',         
+				'type' : 'inline',
+				'showCloseButton' : false,
+				'hideOnOverlayClick' : false,
+				'hideOnContentClick' : false    
+			}), 5000 );  
+			//return $('#formCreateDiscount').valid(); 
+		} 
+														     
+		// post-submit callback 
+		function showResponse(responseText, statusText, xhr, $form)  {    
+			jQuery.fancybox({
+				'content' : '<h1>'+ responseText + '</h1>',
+				'autoScale' : true
+			});
+		} 						
 </script>
 @stop
