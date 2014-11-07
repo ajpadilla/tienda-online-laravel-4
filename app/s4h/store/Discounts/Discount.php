8c6047c39eddd4123a295f1a3fa3b0d3f34748dd
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-07 12:26:46 -0430

    Agregando metodo discountType, para relacionar al descuento con el tipo de descuento corespondiente

--- app/s4h/store/Discounts/Discount.php
+++ app/s4h/store/Discounts/Discount.php
@@ -7,4 +7,8 @@ class Discount extends Eloquent {
 	protected $table = 'discounts';
 
 	protected $fillable = ['name','description','value','percent','quantity','quantity_per_user','code','active','from','to','discount_type_id'];
+
+	public function discountType(){
+		return $this->belongsTo('s4h\store\Discounts_types\DiscountType');
+	}
 }
