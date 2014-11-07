24eec84e6f145af78324885301bc637d3b492d1d
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-07 00:37:14 -0430

    Almacenando datos de los descuentos, en el metodo store

--- app/controllers/DiscountController.php
+++ app/controllers/DiscountController.php
@@ -2,17 +2,21 @@
 
 use s4h\store\Discounts\Discount;
 use s4h\store\Discounts\DiscountRepository;
+use s4h\store\Discounts_types\DiscountTypeRepository;
 use s4h\store\Forms\RegisterDiscountForm;
+use Laracasts\Validation\FormValidationException;
 
 class DiscountController extends \BaseController {
 
 	private $registerDiscountForm;
 	private $discountRepository;
+	private $discountTypeRepository;
 
-	function __construct(RegisterDiscountForm $registerDiscountForm,DiscountRepository $discountRepository)
+	function __construct(RegisterDiscountForm $registerDiscountForm,DiscountRepository $discountRepository, DiscountTypeRepository $discountTypeRepository)
 	{
 		$this->registerDiscountForm = $registerDiscountForm;
 		$this->discountRepository = $discountRepository;	
+		$this->discountTypeRepository = $discountTypeRepository;
 	}
 
 	/**
@@ -33,7 +37,8 @@ class DiscountController extends \BaseController {
 	 */
 	public function create()
 	{
-		return View::make('discounts.create');
+		$discountTypes = $this->discountTypeRepository->getAll()->lists('name', 'id');
+		return View::make('discounts.create',compact('discountTypes'));
 	}
 
 
@@ -44,7 +49,21 @@ class DiscountController extends \BaseController {
 	 */
 	public function store()
 	{
-		dd(Input::all());
+		//dd(Input::all());
+		if(Request::ajax())
+		{
+			$input = Input::all();
+			try
+			{
+				$this->registerDiscountForm->validate($input);
+				$this->createNewDiscount($input);
+				return Response::json('Descuento'.' '.$input['name'].' '.'Agregado con exito!');
+			}
+			catch (FormValidationException $e)
+			{
+				return Response::json($e->getErrors()->all());
+			}
+		}
 	}
 
 
@@ -95,5 +114,21 @@ class DiscountController extends \BaseController {
 		//
 	}
 
+	public function createNewDiscount($data = array())
+	{
+		$discount = new Discount;
+		$discount->name = $data['name'];
+		$discount->description = $data['description'];
+		$discount->value = $data['value'];
+		$discount->percent = $data['percent'];
+		$discount->quantity = $data['quantity'];
+		$discount->quantity_per_user = $data['quantity_per_user'];
+		$discount->code = $data['code'];
+		$discount->active = $data['active'];
+		$discount->from = $data['from'];
+		$discount->to = $data['to'];
+		$discount->discount_type_id = $data['discount_type_id'];
+		$this->discountRepository->save($discount);
+	}
 
 }
