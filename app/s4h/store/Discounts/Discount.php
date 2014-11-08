46af70eb36868b0e323ea2c7ca8bff0fce98480e
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-08 17:03:00 -0430

    Actualizando modelo

--- app/s4h/store/Discounts/Discount.php
+++ app/s4h/store/Discounts/Discount.php
@@ -6,7 +6,7 @@ class Discount extends Eloquent {
 	
 	protected $table = 'discounts';
 
-	protected $fillable = ['name','description','value','percent','quantity','quantity_per_user','code','active','from','to','discount_type_id'];
+	protected $fillable = ['value','percent','quantity','quantity_per_user','code','active','from','to','discount_type_id'];
 
 
 	public function getActivoShow() {
