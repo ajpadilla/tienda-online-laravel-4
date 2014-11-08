6c22df79b48a6ffeb2ff8d4b9426067618ac25a2
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-08 16:58:55 -0430

    Agregando migracion para la tabla lenguage, donde se almacenaran los idiomas del sistema

---
+++ app/database/migrations/2014_11_08_184156_create_languages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('languages', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 255);
+			$table->string('native_name', 255);
+			$table->string('iso_code', 2);
+			$table->string('language_code', 5);
+			$table->string('date_format', 45);
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('languages');
+	}
+
+}
