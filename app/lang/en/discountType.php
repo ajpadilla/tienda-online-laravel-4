81002b85b013f36d4a299155a57b1ebadb66da1d
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-09 16:10:33 -0430

    Agregando traducciones para los tipos de descuentos

---
+++ app/lang/en/discountType.php
@@ -0,0 +1,24 @@
+<?php 
+	return array(
+		"create" => "crear/TypoDescuento",
+		"store" => "agregar/TypoDescuento",
+		"title" => "Agregar Tipo de descuento",
+		"subtitle" => "Crear nuevo  tipo de descuento",
+		"labels" => array(
+			'name' =>'Nombre:',
+			'save' => 'Agregar',
+		),
+		"validation" => array(
+			'required' => 'Campo obligatorio.',
+			'rangelength' => 'Por favor, introduzca un valor entre ',
+			'characters' => 'caracteres.',
+			'number' => 'Por favor, introduzca un número válido.',
+			'digits' => 'Por favor, ingrese sólo dígitos enteros.',
+			'date' => 'Por favor, Ingrese una fecha valida.',
+			'onlyLettersNumbersAndSpaces' => 'Ingrese solo letras, numeros y espacios en blanco',
+			'onlyLettersNumbersAndDash' => 'Ingrese solo letras, numeros y/o guiones.'
+		),
+		'message1' =>'Nuevo tipo de descuento',
+		'message2' =>'Agregado con exito!.',
+		'alert' => '¡El nombre ya se encuentra registrado!'
+	);
