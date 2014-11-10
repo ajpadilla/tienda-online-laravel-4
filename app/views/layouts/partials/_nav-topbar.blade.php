0cce781320af98cc07be12d2d8f180f2bb18f67f
Lenyn Alcantara <nightzpy@gmail.com>
2014-11-10 07:53:22 +0000

    Modificaciones al menú

--- app/views/layouts/partials/_nav-topbar.blade.php
+++ app/views/layouts/partials/_nav-topbar.blade.php
@@ -2,9 +2,16 @@
     <nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
             <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-                <form role="search" class="navbar-form-custom" method="post" action="#">
-                    <div class="input-group" style="margin-top:13px;"><input type="text" class="form-control" placeholder="{{ Lang::get('menu.search') }}" name="top-search" id="top-search"> <span class="input-group-btn"> <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button> </span></div>
-                </form>
+            <form role="search" class="navbar-form-custom" method="post" action="#">
+                <div class="input-group" style="margin-top:13px;">
+                    <input type="text" class="form-control" placeholder="{{ Lang::get('menu.search') }}" name="top-search" id="top-search">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
         </div>
         <ul class="nav navbar-top-links navbar-right">
              <li>
@@ -13,11 +20,45 @@
                 </select>
             </li>
             <li>
-                <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
+                <span class="m-r-sm text-muted welcome-message">Bienvenido a nuestro store.</span>
+            </li>
+			<li class="dropdown">
+                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                    <i class="fa fa-flag"> {{ Lang::get('menu.language') }}</i>
+                    {{--<span class="label label-warning">16</span>--}}
+                </a>
+                <ul class="dropdown-menu dropdown-messages">
+                    <li>
+                        <div class="dropdown-messages-box">
+                            {{--<a href="profile.html" class="pull-left">
+                                <img alt="image" class="img-circle" src="img/a7.jpg">
+                            </a>--}}
+                            <div class="media-body">
+                                <small class="pull-right">Otro Idioma</small>
+                                {{--<strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
+                                <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>--}}
+                            </div>
+                        </div>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <div class="dropdown-messages-box">
+                            {{--<a href="profile.html" class="pull-left">
+                                <img alt="image" class="img-circle" src="img/a4.jpg">
+                            </a>--}}
+                            <div class="media-body ">
+                                <small class="pull-right text-navy">Otro Idioma</small>
+                                {{--<strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
+                                <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>--}}
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </li>
             <li class="dropdown">
                 <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                    <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
+                    <i class="fa fa-shopping-cart"></i>
+                    <span class="label label-warning">16</span>
                 </a>
                 <ul class="dropdown-menu dropdown-messages">
                     <li>
@@ -42,19 +83,6 @@
                                 <small class="pull-right text-navy">5h ago</small>
                                 <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
                                 <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <div class="dropdown-messages-box">
-                            <a href="profile.html" class="pull-left">
-                                <img alt="image" class="img-circle" src="img/profile.jpg">
-                            </a>
-                            <div class="media-body ">
-                                <small class="pull-right">23h ago</small>
-                                <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
-                                <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
                             </div>
                         </div>
                     </li>
