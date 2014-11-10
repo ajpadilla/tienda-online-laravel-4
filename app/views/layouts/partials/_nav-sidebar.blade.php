d9d207abb5f0369184447f6c0e2fa82f875933fa
Lenyn Alcantara <nightzpy@gmail.com>
2014-11-10 08:20:53 +0000

    partial para el perfil

--- app/views/layouts/partials/_nav-sidebar.blade.php
+++ app/views/layouts/partials/_nav-sidebar.blade.php
@@ -2,29 +2,13 @@
             <div class="sidebar-collapse">
                 <ul class="nav" id="side-menu">
                     <li class="nav-header">
-
-                        <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src="{{ asset('assets/img/profile_small.jpg') }}">
-                             </span>
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Amelia Smith</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
-                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a href="profile.html">Profile</a></li>
-                                <li><a href="contacts.html">Contacts</a></li>
-                                <li><a href="mailbox.html">Mailbox</a></li>
-                                <li class="divider"></li>
-                                <li><a href="login.html">Logout</a></li>
-                            </ul>
-                        </div>
+                        @include('layouts.partials._profile-element')
                         <div class="logo-element">
                             IN+
                         </div>
-
                     </li>
                     <li>
                         <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span> </a>
-
                     </li>
 
                     <li>
