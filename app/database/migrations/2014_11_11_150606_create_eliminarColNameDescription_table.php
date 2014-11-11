01de0011bd8fd4a7a1f8e6bca47dddbec93d25a3
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-11 12:56:36 -0430

    Migracion, para eliminar columnas name y description de la tabal discounts

---
+++ app/database/migrations/2014_11_11_150606_create_eliminarColNameDescription_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEliminarColNameDescriptionTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('discounts', function($table)
+		{
+			$table->dropColumn('name');
+			$table->dropColumn('description');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
