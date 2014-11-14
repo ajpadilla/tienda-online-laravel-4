fe99fb746db1912ab7e7319e078751e1d7ce1fd4
Lenyn Alcantara <nightzpy@gmail.com>
2014-11-14 03:44:15 +0000

    Arreglando lenguage

--- app/controllers/DiscountController.php
+++ app/controllers/DiscountController.php
@@ -1,13 +1,13 @@
 <?php
 
-use s4h\store\Discounts\Discount;
-use s4h\store\Discounts\DiscountRepository;
+use Laracasts\Validation\FormValidationException;
+use s4h\store\DiscountsLang\DiscountLangRepository;
 use s4h\store\DiscountsTypes\DiscountType;
 use s4h\store\DiscountsTypes\DiscountTypeRepository;
+use s4h\store\Discounts\Discount;
+use s4h\store\Discounts\DiscountRepository;
 use s4h\store\Forms\RegisterDiscountForm;
-use Laracasts\Validation\FormValidationException;
-use s4h\store\languages\LanguageRepository;
-use s4h\store\DiscountsLang\DiscountLangRepository;
+use s4h\store\Languages\LanguageRepository;
 
 class DiscountController extends \BaseController {
 
@@ -17,9 +17,7 @@ class DiscountController extends \BaseController {
 	private $languageRepository;
 	private $discountLangRepository;
 
-	function __construct(RegisterDiscountForm $registerDiscountForm,DiscountRepository $discountRepository, DiscountTypeRepository $discountTypeRepository, LanguageRepository $languageRepository,DiscountLangRepository  $discountLangRepository)
-
-	{
+	function __construct(RegisterDiscountForm $registerDiscountForm, DiscountRepository $discountRepository, DiscountTypeRepository $discountTypeRepository, LanguageRepository $languageRepository, DiscountLangRepository $discountLangRepository) {
 		$this->registerDiscountForm = $registerDiscountForm;
 		$this->discountRepository = $discountRepository;
 		$this->discountTypeRepository = $discountTypeRepository;
@@ -32,48 +30,39 @@ class DiscountController extends \BaseController {
 	 *
 	 * @return Response
 	 */
-	public function index()
-	{
+	public function index() {
 		return View::make('discounts.index');
 	}
-
 
 	/**
 	 * Show the form for creating a new resource.
 	 *
 	 * @return Response
 	 */
-	public function create()
-	{
+	public function create() {
 		$discountTypes = $this->discountTypeRepository->getNameForLanguage();
-		$languages = $this->languageRepository->getAll()->lists('name','id');
-		return View::make('discounts.create',compact('discountTypes','languages'));
+		$languages = $this->languageRepository->getAll()->lists('name', 'id');
+		return View::make('discounts.create', compact('discountTypes', 'languages'));
 	}
-
 
 	/**
 	 * Store a newly created resource in storage.
 	 *
 	 * @return Response
 	 */
-	public function store()
-	{
-		if(Request::ajax())
-		{
+	public function store() {
+		if (Request::ajax()) {
 			$input = Input::all();
 			try
 			{
 				$this->registerDiscountForm->validate($input);
 				/*$this->discountRepository->createNewDiscount($input);
-				return Response::json(trans('discounts.message1').' '.$input['name'].' '.trans('discounts.message2'));*/
-			}
-			catch (FormValidationException $e)
-			{
+			return Response::json(trans('discounts.message1').' '.$input['name'].' '.trans('discounts.message2'));*/
+			} catch (FormValidationException $e) {
 				return Response::json($e->getErrors()->all());
 			}
 		}
 	}
-
 
 	/**
 	 * Display the specified resource.
@@ -81,13 +70,12 @@ class DiscountController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function show($id)
-	{
+	public function show($id) {
 		$discount = $this->discountRepository->getDiscountId($id);
 		$language_id = $this->languageRepository->returnLanguage()->id;
-		$discount_language = $discount->languages()->where('language_id','=',$language_id)->first();
+		$discount_language = $discount->languages()->where('language_id', '=', $language_id)->first();
 		$discountTypes = $this->discountTypeRepository->getNameForLanguage();
-		return View::make('discounts.show',compact('discount','discount_language','discountTypes'));
+		return View::make('discounts.show', compact('discount', 'discount_language', 'discountTypes'));
 	}
 
 	/**
@@ -96,17 +84,15 @@ class DiscountController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function edit($id)
-	{
+	public function edit($id) {
 		$discount = $this->discountRepository->getDiscountId($id);
 		$language_id = $this->languageRepository->returnLanguage()->id;
-		$discount_language = $discount->languages()->where('language_id','=',$language_id)->first();
+		$discount_language = $discount->languages()->where('language_id', '=', $language_id)->first();
 		$discountTypes = $this->discountTypeRepository->getNameForLanguage();
-		$languages = $this->languageRepository->getAll()->lists('name','id');
-		return View::make('discounts.edit',compact('discount','discount_language','discountTypes','languages'));
+		$languages = $this->languageRepository->getAll()->lists('name', 'id');
+		return View::make('discounts.edit', compact('discount', 'discount_language', 'discountTypes', 'languages'));
 
 	}
-
 
 	/**
 	 * Update the specified resource in storage.
@@ -114,25 +100,20 @@ class DiscountController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
-	{
-		if(Request::ajax())
-		{
+	public function update($id) {
+		if (Request::ajax()) {
 			$input = Input::all();
 			//dd($input);
 			try
 			{
 				//$this->registerDiscountForm->validate($input);
 				$this->discountRepository->updateDiscount($input, $id);
-				return Response::json(trans('discounts.message1').' '.$input['name'].' '.trans('discounts.message2'));
-			}
-			catch (FormValidationException $e)
-			{
+				return Response::json(trans('discounts.message1') . ' ' . $input['name'] . ' ' . trans('discounts.message2'));
+			} catch (FormValidationException $e) {
 				return Response::json($e->getErrors()->all());
 			}
 		}
 	}
-
 
 	/**
 	 * Remove the specified resource from storage.
@@ -140,66 +121,56 @@ class DiscountController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function destroy($id)
-	{
+	public function destroy($id) {
 		$this->discountRepository->deleteDiscount($id);
 	}
 
-	public function getDatatable()
-	{
+	public function getDatatable() {
 		$collection = Datatable::collection($this->discountLangRepository->getAllForLanguage($this->languageRepository->returnLanguage()->id))
-			
-			->searchColumns('name','code','discount_type_id','name','value','percent','active','from','to')
-			->orderColumns('code');
 
-		$collection->addColumn('code', function($model)
-		{
+		->searchColumns('name', 'code', 'discount_type_id', 'name', 'value', 'percent', 'active', 'from', 'to')
+		->orderColumns('code');
+
+		$collection->addColumn('code', function ($model) {
 			return $model->discount->code;
 		});
 
-		$collection->addColumn('discount_type_id', function($model)
-		{
-			  foreach ($model->discount->discountType->languages as $language) {
-			  	return $language->pivot->name;
-			  }
+		$collection->addColumn('discount_type_id', function ($model) {
+			foreach ($model->discount->discountType->languages as $language) {
+				return $language->pivot->name;
+			}
 		});
 
-		$collection->addColumn('name', function($model)
-		{
+		$collection->addColumn('name', function ($model) {
 			return $model->name;
 		});
-	
-		$collection->addColumn('value', function($model)
-		{
+
+		$collection->addColumn('value', function ($model) {
 			return $model->discount->value;
 		});
 
-		$collection->addColumn('percent', function($model)
-		{
+		$collection->addColumn('percent', function ($model) {
 			return $model->discount->percent;
 		});
 
-		$collection->addColumn('active', function($model)
-		{
+		$collection->addColumn('active', function ($model) {
 			return $model->discount->getActivoShow();
 		});
 
-		$collection->addColumn('from', function($model)
-		{
-			return date( trans('discounts.date2') ,strtotime($model->discount->from));
+		$collection->addColumn('from', function ($model) {
+			return date(trans('discounts.date2'), strtotime($model->discount->from));
 		});
 
-		$collection->addColumn('to', function($model)
-		{
-			return date(trans('discounts.date2') ,strtotime($model->discount->to));
+		$collection->addColumn('to', function ($model) {
+			return date(trans('discounts.date2'), strtotime($model->discount->to));
 		});
 
-		$collection->addColumn('Actions',function($model){
-			$links = "<a href='" .route('discounts.show',$model->discount->id). "'>View</a>
+		$collection->addColumn('Actions', function ($model) {
+			$links = "<a href='" . route('discounts.show', $model->discount->id) . "'>View</a>
 					<br />";
-			$links .= "<a href='" .route('discounts.edit',$model->discount->id). "'>Edit</a>
+			$links .= "<a href='" . route('discounts.edit', $model->discount->id) . "'>Edit</a>
 					<br />
-					<a href='" .route('discounts.destroy',$model->discount->id). "'>Delete</a>";
+					<a href='" . route('discounts.destroy', $model->discount->id) . "'>Delete</a>";
 
 			return $links;
 		});
@@ -207,28 +178,25 @@ class DiscountController extends \BaseController {
 		return $collection->make();
 	}
 
-	public function checkCode()
-	{
-		if(Request::ajax())
-		{
+	public function checkCode() {
+		if (Request::ajax()) {
 			$discount = $this->discountRepository->getCode(Input::get('code'));
-			if(count($discount) > 0){
+			if (count($discount) > 0) {
 				return Response::json(false);
-			}else{
-				 return Response::json(true);
+			} else {
+				return Response::json(true);
 			}
-       	}
+		}
 		return Response::json(array('respuesta' => 'false'));
 	}
 
-	public function checkCodeForEdit()
-	{
+	public function checkCodeForEdit() {
 		if (Request::ajax()) {
 			$discount = $this->discountRepository->getCodeEdit(Input::all());
-			if(count($discount) > 0){
+			if (count($discount) > 0) {
 				return Response::json(false);
-			}else{
-				 return Response::json(true);
+			} else {
+				return Response::json(true);
 			}
 		}
 	}
