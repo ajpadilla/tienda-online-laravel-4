9ca39d673bfd1456e49899c4ab98180a0d699069
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-15 16:23:48 -0430

    Actualizando repositorio

--- app/s4h/store/shipmentStatus/ShipmentStatusRepository.php
+++ app/s4h/store/shipmentStatus/ShipmentStatusRepository.php
@@ -1,11 +1,13 @@
 <?php namespace s4h\store\ShipmentStatus;
 
+use s4h\store\ShipmentStatus\ShipmentStatus;
+
 /**
 * 
 */
 class ShipmentStatusRepository extends AnotherClass
 {
-	public function save($value='')
+	public function save()
 	{
 
 	}	
