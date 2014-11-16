d8f9d2e871aed97b071d7e98ae3344a88461d2fa
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-16 15:24:28 -0430

    Agregando metodos getNameShipmentStatus y getColor

--- app/s4h/store/ShipmentStatus/ShipmentStatusRepository.php
+++ app/s4h/store/ShipmentStatus/ShipmentStatusRepository.php
@@ -1,7 +1,7 @@
 <?php namespace s4h\store\ShipmentStatus;
 
 use s4h\store\ShipmentStatus\ShipmentStatus;
-
+use s4h\store\Languages\Language;
 /**
 * 
 */
@@ -18,4 +18,21 @@ class ShipmentStatusRepository {
 		$shipmentStatus->save();
 		$shipmentStatus->languages()->attach($data['language_id'], array('name' => $data['name'], 'description' => $data['description']));
 	}	
+
+	public function getNameShipmentStatus($data = array())
+	{
+		$language = Language::select()->where('id','=',$data['language_id'])->first();
+		if (count($language) > 0) {
+			return $language->shipment_status()->wherePivot('name','=',$data['name'])->first();
+		}else{
+			return $language;
+		}
+	}
+
+	public function getColor($color)
+	{
+		$shipment_status = ShipmentStatus::select()->where('color','=',$color))->first();
+		return $shipment_status;
+	}
+	
 }
