c11ec4942aa5c480bebce9c58e4eff13a1a11b08
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-17 17:41:42 -0430

    asociando el objeto language, con el metodo invoice_status

--- app/s4h/store/InvoiceStatus/InvoiceStatusRepository.php
+++ app/s4h/store/InvoiceStatus/InvoiceStatusRepository.php
@@ -1,6 +1,7 @@
 <?php namespace s4h\store\InvoiceStatus;
 
 use s4h\store\InvoiceStatus\InvoiceStatus;
+use s4h\store\Languages\Language;
 /**
 * 
 */
@@ -18,5 +19,15 @@ class InvoiceStatusRepository {
 		$invoiceStatus->save();
 		$invoiceStatus->languages()->attach($data['language_id'], array('name' => $data['name'], 'description' => $data['description']));
 	}	
+
+	public function getNameInvoiceStatus($data = array())
+	{
+		$language = Language::select()->where('id','=',$data['language_id'])->first();
+		if (count($language) > 0) {
+			return $language->invoice_status()->wherePivot('name','=',$data['name'])->first();
+		}else{
+			return $language;
+		}
+	}
 
 }
