3fb8be294842099233b041e4377c6316b52c4933
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-18 17:03:32 -0430

    Agregandos discunt_types y language

--- app/s4h/store/DiscountTypesLang/DiscountTypeLang.php
+++ app/s4h/store/DiscountTypesLang/DiscountTypeLang.php
@@ -6,4 +6,12 @@ class DiscountTypeLang extends Eloquent {
 	
 	protected $table = 'discount_types_lang';
 
+	public function discount_types(){
+		return $this->belongsTo('s4h\store\DiscountsTypes\DiscountType','discount_type_id');
+	}
+
+	public function language(){
+		return $this->belongsTo('s4h\store\Languages\Language','language_id');
+	}
+
 }
