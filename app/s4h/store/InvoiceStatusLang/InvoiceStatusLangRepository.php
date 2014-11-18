068c9fa83812bb425ab8922ebf33bb6df31a309c
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-17 20:59:01 -0430

    Agregando metodo getAllForLanguage

--- app/s4h/store/InvoiceStatusLang/InvoiceStatusLangRepository.php
+++ app/s4h/store/InvoiceStatusLang/InvoiceStatusLangRepository.php
@@ -1,9 +1,14 @@
-<?php namespace s4h\store\InvoiceStatusLang\;
+<?php namespace s4h\store\InvoiceStatusLang;
  
  use s4h\store\InvoiceStatusLang\InvoiceStatusLang;
  /**
  * 
  */
  class InvoiceStatusLangRepository {
- 	
+ 
+ 	public function getAllForLanguage($language_id)
+  	{
+  		return InvoiceStatusLang::where('language_id','=',$language_id)->get();
+  	}
+
  }
