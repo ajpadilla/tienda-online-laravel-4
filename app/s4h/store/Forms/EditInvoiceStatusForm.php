daf2cf2f2167a04217b4c964d36f78fab412993b
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-18 00:30:02 -0430

    Nueva clase para validar de un objeto de tipo invoice_estatus al actualizar

---
+++ app/s4h/store/Forms/EditInvoiceStatusForm.php
@@ -0,0 +1,11 @@
+<?php namespace s4h\store\Forms;
+
+use Laracasts\Validation\FormValidator;
+
+class EditInvoiceStatusForm extends FormValidator{
+	protected $rules = [
+                    'name' => 'required|max:255',
+                    'description' => 'required',
+                    'color' => 'required|max:255'
+     ];
+}
