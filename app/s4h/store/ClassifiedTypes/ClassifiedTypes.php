d811f5cacfe1a4c37c57e789db5d79de4587bc6f
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-19 17:15:37 -0430

    Agregando clase y repositorio para el modelo classified_types

---
+++ app/s4h/store/ClassifiedTypes/ClassifiedTypes.php
@@ -0,0 +1,20 @@
+<?php namespace s4h\store\ClassifiedTypes;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+/**
+* 
+*/
+class ClassifiedTypes extends Eloquent {
+	
+	use SoftDeletingTrait;
+
+    protected $dates = ['deleted_at'];
+
+	protected $table = 'classified_types';
+
+	public function languages(){
+		return $this->belongsToMany('s4h\store\Languages\Language','classified_types_lang','classified_types_id','language_id')->withPivot('name');
+	}	
+
+}
---
+++ app/s4h/store/ClassifiedTypes/ClassifiedTypesRepository.php
@@ -0,0 +1,9 @@
+<?php namespace s4h\store\ClassifiedTypes;
+
+/**
+* 
+*/
+class ClassifiedTypesRepository {
+	
+	
+}
