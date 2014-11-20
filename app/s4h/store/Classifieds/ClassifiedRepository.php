fb4d5c460c9fdc2dc22b338ebbd3e3987b755a98
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-20 15:58:41 -0430

    Agregando metodo createNewClassifieds

---
+++ app/s4h/store/Classifieds/ClassifiedRepository.php
@@ -0,0 +1,26 @@
+<?php namespace s4h\store\Classifieds;
+
+use s4h\store\Classifieds\Classified;
+use s4h\store\Languages\Language;
+use s4h\store\Photos\ClassifiedPhotos;
+
+/**
+* 
+*/
+class ClassifiedRepository{
+
+	public function createNewClassified($data = array())
+	{
+		$classified = new Classified;
+		$classified->price = $data['price'];
+		$classified->user_id = $data['price'];
+		$classified->classified_type_id = $data['classified_type_id'];
+		$classified->classified_condition_id = $data['classified_type_id'];
+		$classified->save();
+		$classified->languages()->attach($data['language_id'], array('name'=> $data['name'],
+			'description'=> $data['description'],
+			'address' => $data['address']
+		));
+	}
+	
+}
