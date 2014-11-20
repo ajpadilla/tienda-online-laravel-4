e0d38a92196a13a3d2a1ce8695f8282ec58ed563
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-19 21:46:05 -0430

    Modificando ruta del archivo para las traducciones del metodo trans

--- app/views/classified_types/show.blade.php
+++ app/views/classified_types/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('title')
 {{--{{ Lang::get('modulo.variable') }}--}}
-{{ trans('discountType.title') }}
+{{ trans('classifiedTypes.title') }}
 @stop
 
 @section('content')
@@ -10,20 +10,20 @@
 	<div class="col-lg-12">
 		<div class="ibox float-e-margins">
 			<div class="ibox-title">
-				<h5>{{ trans('discountType.subtitle') }}</h5>
+				<h5>{{ trans('classifiedTypes.subtitle') }}</h5>
 			</div>
 			<div class="ibox-content">
 				<div class="row">
-					{{ Form::open(['url' => LaravelLocalization::transRoute('discountType.store'),'class'=>'form-horizontal','method' => 'POST','id' => 'formCreateDiscountType']) }}
+					{{ Form::open(['url' => LaravelLocalization::transRoute('classifiedTypes.store'),'class'=>'form-horizontal','method' => 'POST','id' => 'formCreateclassifiedTypes']) }}
 					<div class="col-sm-6 b-r">
 						<div class="form-group">
-							{{ Form::label('language_id', trans('discountType.labels.language'),['class'=>'col-sm-2 control-label']) }}
+							{{ Form::label('language_id', trans('classifiedTypes.labels.language'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-8">
 								{{ Form::text('language_id',$classified_type_language->name, ['class' => 'form-control','id'=>'language_id','readonly']) }}
 							</div>
 						</div>
 						<div class="form-group">
-							{{ Form::label('name',trans('discountType.labels.name'),['class'=>'col-sm-2 control-label']) }}
+							{{ Form::label('name',trans('classifiedTypes.labels.name'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-8">
 								{{ Form::text('name',$classified_type_language->pivot->name, ['class' => 'form-control','id'=>'name','readonly']) }}
 							</div>
