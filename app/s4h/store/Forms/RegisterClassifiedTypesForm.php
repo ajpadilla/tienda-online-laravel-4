3928b28873c378958880c3ba7342bb08dfa6de7e
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-19 20:54:55 -0430

    agregando reglas para validar campos del modelo classified_types

--- app/s4h/store/Forms/RegisterClassifiedTypesForm.php
+++ app/s4h/store/Forms/RegisterClassifiedTypesForm.php
@@ -1,1 +1,15 @@
-<?php 
+<?php namespace s4h\store\Forms;
+
+use Laracasts\Validation\FormValidator;
+
+/**
+* 
+*/
+class RegisterClassifiedTypesForm extends FormValidator {
+
+		protected $rules = [
+			'name' => 'required|max:128',
+		];
+	
+}
+
