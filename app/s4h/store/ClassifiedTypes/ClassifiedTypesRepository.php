373031dd19c557ced438eb906037299fd90ed2af
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-19 20:34:07 -0430

    Agregando metodo save, getAlly createNewClassifiedType

--- app/s4h/store/ClassifiedTypes/ClassifiedTypesRepository.php
+++ app/s4h/store/ClassifiedTypes/ClassifiedTypesRepository.php
@@ -1,9 +1,23 @@
 <?php namespace s4h\store\ClassifiedTypes;
 
+use s4h\store\ClassifiedTypes\ClassifiedType;
 /**
 * 
 */
 class ClassifiedTypesRepository {
 	
-	
+	public function save(ClassifiedType $classifiedType){
+		return $classifiedType->save();
+	}
+
+	public function getAll(){
+		return ClassifiedType::all();
+	}
+
+	public function createNewClassifiedType($data = array())
+	{
+		$classified_type = new ClassifiedType;
+		$classified_type->save();
+		$classified_type->languages()->attach($data['language_id'], array('name'=> $data['name']));
+	}
 }
