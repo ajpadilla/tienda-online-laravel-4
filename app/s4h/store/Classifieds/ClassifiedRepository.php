0c61607953f4d5f67b9189e80d6cf95c7e47a3d8
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-20 16:28:18 -0430

    Actualizando clase

--- app/s4h/store/Classifieds/ClassifiedRepository.php
+++ app/s4h/store/Classifieds/ClassifiedRepository.php
@@ -15,7 +15,7 @@ class ClassifiedRepository{
 		$classified->price = $data['price'];
 		$classified->user_id = $data['price'];
 		$classified->classified_type_id = $data['classified_type_id'];
-		$classified->classified_condition_id = $data['classified_type_id'];
+		$classified->classified_condition_id = $data['classified_condition_id'];
 		$classified->save();
 		$classified->languages()->attach($data['language_id'], array('name'=> $data['name'],
 			'description'=> $data['description'],
