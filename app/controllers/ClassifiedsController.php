b0e6373aaa6bcfdcd28930afdbb2c6b81f31deda
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-20 15:55:48 -0430

    Agregando controlador para el modelo classifieds

---
+++ app/controllers/ClassifiedsController.php
@@ -0,0 +1,95 @@
+<?php
+
+use Laracasts\Validation\FormValidationException;
+use s4h\store\Languages\LanguageRepository;
+use s4h\store\Classifieds\ClassifiedRepository;
+
+class ClassifiedsController extends \BaseController {
+	private $classifiedRepository;
+
+	function __construct(ClassifiedRepository $classifiedRepository) {
+		$this->$classifiedRepository = $classifiedRepository;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
