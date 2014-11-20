2792c0da1f5ac1641fd16159679579c10e22b6b9
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-20 14:55:08 -0430

    Modificando migracion para la tabla classifiend_photos

--- app/database/migrations/2014_11_20_053138_create_classified_photo_table.php
+++ app/database/migrations/2014_11_20_053138_create_classified_photo_table.php
@@ -17,9 +17,11 @@ class CreateClassifiedPhotoTable extends Migration {
 			$table->increments('id');
             $table->string('filename');
             $table->string('path');
+	        $table->string('complete_path');
+	        $table->string('complete_thumbnail_path');
             $table->integer('size');
-            $table->string('extension');
-            $table->string('mimetype');
+            $table->string('extension', 3);
+            $table->string('mimetype', 32);
             $table->integer('user_id')->unsigned()->index();
             $table->integer('classified_id')->unsigned()->index(); // If this is a child file, it'll be referenced here.
             $table->softDeletes();
