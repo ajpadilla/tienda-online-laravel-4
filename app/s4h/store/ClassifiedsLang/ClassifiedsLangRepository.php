8000b380f1baa423ab37d671fecfd46fc74214a5
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-21 16:49:25 -0430

    Agregando clase repository para el modelo classifieds_lang

--- app/s4h/store/ClassifiedsLang/ClassifiedsLangRepository.php
+++ app/s4h/store/ClassifiedsLang/ClassifiedsLangRepository.php
@@ -1,10 +1,15 @@
 <?php namespace s4h\store\ClassifiedsLang;
 
+use s4h\store\ClassifiedsLang\ClassifiedsLang;
 
 /**
 * 
 */
-class ClassifiedLangRepository{
+class ClassifiedsLangRepository{
 
+	public function getAllForLanguage($language_id)
+	{
+		return ClassifiedsLang::where('language_id','=',$language_id)->get();
+	}
 
 }
