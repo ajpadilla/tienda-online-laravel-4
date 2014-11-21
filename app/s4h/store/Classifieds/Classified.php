fa457d9036cdab12cd1453cf089ce81513544019
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-21 16:48:00 -0430

    Agregando metodos classifiedTyep, classifiedCondition

--- app/s4h/store/Classifieds/Classified.php
+++ app/s4h/store/Classifieds/Classified.php
@@ -17,4 +17,16 @@ class Classified extends Eloquent {
 		return $this->belongsToMany('s4h\store\Languages\Language','classifieds_lang','classified_id','language_id')->withPivot('name','description','address');
 	}	
 
+	public function user(){
+		return $this->belongsTo('s4h\store\Users\User','user_id');
+	}
+
+	public function classifiedType(){
+		return $this->belongsTo('s4h\store\ClassifiedTypes\ClassifiedType','classified_type_id');
+	}
+
+	public function classifiedCondition(){
+		return $this->belongsTo('s4h\store\ClassifiedConditions\ClassifiedCondition','classified_condition_id');
+	}
+
 }
