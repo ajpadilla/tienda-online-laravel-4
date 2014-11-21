d27244725cd6aaa2b69196a472787dd22dfdc0b2
Lenyn Alcantara <nightzpy@gmail.com>
2014-11-21 03:59:17 +0000

    Ultimas cambios

--- app/config/database.php
+++ app/config/database.php
@@ -48,10 +48,10 @@ return array(
 
 		'mysql' => array(
 			'driver' => 'mysql',
-			'host' => getenv('DB_HOST'),
-			'database' => getenv('DB_NAME'),
-			'username' => getenv('DB_USERNAME'),
-			'password' => getenv('DB_PASSWORD'),
+			'host' => 'localhost',
+			'database' => 'nightzpy_tonline',
+			'username' => 'nightzpy_tonline',
+			'password' => 'tonline',
 			'charset' => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix' => '',
