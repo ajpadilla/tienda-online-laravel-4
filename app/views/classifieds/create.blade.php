aac920368d8da0056b3c57790f554d5775d5521a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-21 15:40:55 -0430

    Actualizando blade

--- app/views/classifieds/create.blade.php
+++ app/views/classifieds/create.blade.php
@@ -14,12 +14,12 @@
 			</div>
 			<div class="ibox-content">
 				<div class="row">
-					{{ Form::open(['url' => LaravelLocalization::transRoute('classifieds.store'),'class'=>'form-horizontal','method' => 'POST','id' => 'formCreateDiscount']) }}
+					{{ Form::open(['url' => LaravelLocalization::transRoute('classifieds.store'),'class'=>'form-horizontal','method' => 'POST','id' => 'formCreateClassified']) }}
 					<div class="col-sm-6 b-r">
 						<div class="form-group">
 							{{ Form::label('language_id', trans('classifieds.labels.language'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-6">
-								{{ Form::select('language_id',array(),null,array('class' => 'form-control','id'=>'language_id')) }}
+								{{ Form::select('language_id',$languages,null,array('class' => 'form-control','id'=>'language_id')) }}
 							</div>
 						</div>
 						
@@ -36,41 +36,47 @@
 							</div>
 						</div>
 						<div class="form-group">
-							{{ Form::label('price', trans('classifieds.labels.price'),['class'=>'col-sm-2 control-label']) }}
+							{{ Form::label('address', trans('classifieds.labels.address'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-6">
-								{{ Form::text('price',null, ['class' => 'form-control']) }}
+								{{ Form::textarea('address',null, ['class' => 'form-control', 'rows' => '3']) }}
 							</div>
 						</div>
 					</div>
 
 					<div class="col-sm-6">
-						
+						<div class="form-group">
+							{{ Form::label('price', trans('classifieds.labels.price'),['class'=>'col-sm-2 control-label']) }}
+							<div class="col-sm-6">
+								{{ Form::text('price',null, ['class' => 'form-control']) }}
+							</div>
+						</div>
+
 						<div class="form-group">
 							{{ Form::label('classified_type_id', trans('classifieds.labels.classified_type'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-6">
-								{{ Form::select('discount_type_id',array(),null,array('class' => 'form-control')) }}
+								{{ Form::select('classified_type_id',$classified_types,null,array('class' => 'form-control')) }}
 							</div>
 						</div>
 						<div class="form-group">
 							{{ Form::label('classified_condition_id', trans('classifieds.labels.classified_condition'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-6">
-								{{ Form::select('classified_condition_id',array(),null,array('class' => 'form-control')) }}
+								{{ Form::select('classified_condition_id',$classified_conditions,null,array('class' => 'form-control')) }}
 							</div>
 						</div>
 						<div class="form-group">
-							{{ Form::label('active', trans('classifieds.labels.add_photos'),['class'=>'col-sm-2 control-label']) }}
+							{{ Form::label('add_photos', trans('classifieds.labels.add_photos'),['class'=>'col-sm-2 control-label']) }}
 							<div class="col-sm-10">
 								<div class="radio i-checks">
 									<label> 
 										<!--<input type="radio" value="option1" name="a">-->
-										{{ Form::radio('active', '1', 1)}}
+										{{ Form::radio('add_photos', '1', 1)}}
 										<i></i> {{ trans('classifieds.labels.Yes') }}
 									</label>
 								</div>
 								<div class="radio i-checks">
 									<label> 
 										<!--<input type="radio" value="option1" name="a">--> 
-										{{ Form::radio('active', '0', 0)}}
+										{{ Form::radio('add_photos', '0', 0)}}
 										<i></i> {{ trans('classifieds.labels.No') }}
 									</label>
 								</div>
@@ -93,25 +99,6 @@
 <script>
 	$(document).ready(function () 
 	{
-		$('.i-checks').iCheck({
-			checkboxClass: 'icheckbox_square-green',
-			radioClass: 'iradio_square-green',
-		});
-
-		$('#from').datepicker({
-			showButtonPanel: true,
-			changeMonth: true,
-			changeYear: true,
-			yearRange: '2014:2030',
-			dateFormat: '{{ trans('classifieds.date') }}',
-		});
-
-		$('#to').datepicker({
-			showButtonPanel: true,
-			changeMonth: true,
-			changeYear: true,
-			dateFormat: '{{ trans('classifieds.date') }}',
-		});
 
 		$.validator.addMethod('onlyLettersNumbersAndSpaces', function(value, element) {
          	  return this.optional(element) || /^[a-zA-Z0-9ñÑ\s]+$/i.test(value);
@@ -127,114 +114,62 @@
         	catch(e){return false;}
     	},'{{ trans('classifieds.validation.date') }}');
 
-		$('#formCreateDiscount').validate({
+		$('#formCreateClassified').validate({
 
 			rules:{
 				name:{
 					required:!0,
 					rangelength: [2, 255],
-					onlyLettersNumbersAndSpaces: true
+					onlyLettersNumbersAndSpaces: true,
+					remote:
+					{
+						url:'{{ URL::to('/checkNameClassified/') }}',
+						type: 'POST',
+						data: {
+							language_id: function() {
+								return $('#language_id').val();
+							},
+							name: function() {
+								return $('#name').val();
+							}
+						},
+						dataFilter: function (respuesta) {
+							console.log('consulta:'+respuesta);
+							return respuesta;
+						}
+					}
 				},
 				description:{
 					required:!0,
 					rangelength: [10, 255]
 				},
-				value:{
+				addres:{
+					required:!0,
+					rangelength: [10, 255]
+				},
+				price:{
 					required:!0,
 					number: true
 				},
-				percent:{
-					required:!0,
-					number: true
-				},
-				quantity:{
-					required:!0,
-					digits: true
-				},
-				quantity_per_user:{
-					required:!0,
-					digits: true
-				},
-				code:{
-					required:!0,
-					onlyLettersNumbersAndDash: true,
-					 remote:
-						{
-							url:'{{ URL::to('/checkCode/') }}',
-							type: 'POST',
-							data: {
-								code: function() {
-									return $('#code').val();
-								}
-							},
-							dataFilter: function (respuesta) {
-								console.log('consulta:'+respuesta);
-								return respuesta;
-							}
-						} 
-				},
-				active:{
-					required:!0,
-					digits: true
-				},
-				from:{
-					required:!0,
-					customDateValidator: true
-				},
-				to:{
-					required:!0,
-					customDateValidator: true
-				},
-				discount_type_id:{
-					required:!0,
-					digits: true
-				}
 			},
 			messages:{
 				name:{
 					required: '{{ trans('classifieds.validation.required') }}',
 					rangelength: '{{ trans('classifieds.validation.rangelength') }}'+'[2, 255]'+'{{ trans('classifieds.validation.characters') }}',
+					remote: jQuery.validator.format('{{ trans('classifieds.alert') }}')
 				},
 				description:{
 					required: '{{ trans('classifieds.validation.required') }}',
 					rangelength: '{{ trans('classifieds.validation.rangelength') }}'+'[10, 255]'+'{{ trans('classifieds.validation.characters') }}',
 				},
-				value:{
+				addres:{
+					required: '{{ trans('classifieds.validation.required') }}',
+					rangelength: '{{ trans('classifieds.validation.rangelength') }}'+'[10, 255]'+'{{ trans('classifieds.validation.characters') }}',
+				},
+				price:{
 					required: '{{ trans('classifieds.validation.required') }}',
 					number: '{{ trans('classifieds.validation.number') }}'
 				},
-				percent:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					number: '{{ trans('classifieds.validation.number') }}'
-				},
-				quantity:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					digits: '{{ trans('classifieds.validation.digits') }}'
-				},
-				quantity_per_user:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					digits: '{{ trans('classifieds.validation.digits') }}'
-				},
-				code:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					remote: jQuery.validator.format('{{ trans('classifieds.alert') }}')
-				},
-				active:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					digits: '{{ trans('classifieds.validation.digits') }}'
-				},
-				from:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					//date: '{{ trans('classifieds.validation.date') }}'
-				},
-				to:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					//date: '{{ trans('classifieds.validation.date') }}'
-				},
-				discount_type_id:{
-					required: '{{ trans('classifieds.validation.required') }}',
-					digits: '{{ trans('classifieds.validation.digits') }}'
-				}
 			},
 			highlight:function(element){
 				$(element).closest('.form-group').removeClass('has-success').addClass('has-error');
@@ -254,13 +189,13 @@
 				url:  '{{URL::route('classifieds.store')}}',
         		type:'POST'
 			};
-		$('#formCreateDiscount').ajaxForm(options);
+		$('#formCreateClassified').ajaxForm(options);
 	});
 
 	// pre-submit callback
 		function showRequest(formData, jqForm, options) {
 			setTimeout(jQuery.fancybox({
-				'content': '<h1>Enviando datos</h1>',
+				'content': '<h1>' + '{{ trans('classifieds.sending') }}' + '</h1>',
 				'autoScale' : true,
 				'transitionIn' : 'none',
 				'transitionOut' : 'none',
@@ -270,7 +205,7 @@
 				'hideOnOverlayClick' : false,
 				'hideOnContentClick' : false
 			}), 5000 );
-			return $('#formCreateDiscount').valid();
+			return $('#formCreateClassified').valid();
 		}
 
 		// post-submit callback
@@ -279,7 +214,6 @@
 				'content' : '<h1>'+ responseText + '</h1>',
 				'autoScale' : true
 			});
-		} 						
-
+		} 			
 </script>
 @stop
