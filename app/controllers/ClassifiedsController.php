2d2d554cde04ce0ca0035bda76635f86d3e18a59
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-21 15:36:37 -0430

    Reorganizando el metodo store del controlador

--- app/controllers/ClassifiedsController.php
+++ app/controllers/ClassifiedsController.php
@@ -3,13 +3,24 @@
 use Laracasts\Validation\FormValidationException;
 use s4h\store\Languages\LanguageRepository;
 use s4h\store\Classifieds\ClassifiedRepository;
+use s4h\store\ClassifiedConditions\ClassifiedConditionsRepository;
+use s4h\store\ClassifiedTypes\ClassifiedTypesRepository;
+use s4h\store\Users\UserRepository;
 
 class ClassifiedsController extends \BaseController {
 
 	private $classifiedRepository;
+	private $classifiedConditionsRepository;
+	private $classifiedTypesRepository;
+	private $userRepository;
+	private $languageRepository;
 
-	function __construct(ClassifiedRepository $classifiedRepository) {
+	function __construct(ClassifiedRepository $classifiedRepository, ClassifiedConditionsRepository $classifiedConditionsRepository, ClassifiedTypesRepository $classifiedTypesRepository, UserRepository $userRepository,LanguageRepository $languageRepository) {
 		$this->classifiedRepository = $classifiedRepository;
+		$this->classifiedConditionsRepository = $classifiedConditionsRepository;
+		$this->classifiedTypesRepository = $classifiedTypesRepository;
+		$this->userRepository = $userRepository;
+		$this->languageRepository = $languageRepository;
 	}
 
 	/**
@@ -30,7 +41,11 @@ class ClassifiedsController extends \BaseController {
 	 */
 	public function create()
 	{
-		return View::make('classifieds.create');
+		$languages = $this->languageRepository->getAll()->lists('name', 'id');
+		$classified_conditions = $this->classifiedConditionsRepository->getNameForLanguage(); 
+		$classified_types = $this->classifiedTypesRepository->getNameForLanguage();
+		$users = $this->userRepository->getAll()->lists('username', 'id');
+		return View::make('classifieds.create', compact('languages','classified_conditions','classified_types','users'));
 	}
 
 
@@ -41,7 +56,17 @@ class ClassifiedsController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$input = Input::all();
+		//dd($input);
+		try
+		{
+			$classified = $this->classifiedRepository->createNewClassified($input);
+			return Response::json(trans('classifieds.message1') . ' ' . $input['name'] . ' ' . trans('classifieds.message2'));
+		} 
+		catch (FormValidationException $e)
+		{
+			return Response::json($e->getErrors()->all());
+		}
 	}
 
 
@@ -92,5 +117,18 @@ class ClassifiedsController extends \BaseController {
 		//
 	}
 
-
+	
+	public function checkNameClassified() 
+	{
+		$response = array();
+		if (Request::ajax()) {
+			$classified = $this->classifiedRepository->getName(Input::all());
+			if (count($classified) > 0) {
+				return Response::json(false);
+			} else {
+				return Response::json(true);
+			}
+		}
+		return Response::json(array('respuesta' => 'false'));
+	}
 }
