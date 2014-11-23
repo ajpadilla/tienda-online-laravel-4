38f9cd74cb7c5968466a7b843aff976d686a4f49
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-23 17:59:13 -0430

    Agregando datos de prueba para la tabla classifieds_types_lnag

---
+++ app/database/seeds/ClassifiedConditionsLangTableSeeder.php
@@ -0,0 +1,53 @@
+<?php 
+
+/**
+* 
+*/
+class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
+	
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$date = new DateTime;
+
+		$classified_conditions_lang [] = array(
+			'name' => 'En espera', 
+			'classified_conditions_id' =>1,
+			'language_id' => 1,  
+			'created_at' => $date->format('Y-m-d h:m:s'),
+			'updated_at' => $date->format('Y-m-d h:m:s')             
+		);  
+
+		$classified_conditions_lang [] = array(
+			'name' => 'On hold',
+			'classified_conditions_id' =>1,
+			'language_id' => 2,
+			'created_at' => $date->format('Y-m-d h:m:s'),
+			'updated_at' => $date->format('Y-m-d h:m:s')    
+		);  
+
+		$classified_conditions_lang [] = array(
+			'name' => 'Aceptado', 
+			'classified_conditions_id' =>2,
+			'language_id' => 1,  
+			'created_at' => $date->format('Y-m-d h:m:s'),
+			'updated_at' => $date->format('Y-m-d h:m:s')             
+		);  
+
+		$classified_conditions_lang [] = array(
+			'name' => 'Accepted',
+			'classified_conditions_id' =>2,
+			'language_id' => 2,
+			'created_at' => $date->format('Y-m-d h:m:s'),
+			'updated_at' => $date->format('Y-m-d h:m:s')    
+		);  
+
+		// Uncomment the below to run the seeder
+		DB::table('classified_conditions_lang')->insert($classified_conditions_lang);
+	}
+
+}
