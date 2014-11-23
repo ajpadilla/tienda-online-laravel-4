eb0987fe5f0b0ff05c83fa6647df687e5e9391ef
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-23 14:22:52 -0430

    Agregando faker

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,20 @@
 
 class DatabaseSeeder extends Seeder {
 
+
+	protected $faker;
+	
+	public function getFaker()
+	{
+		if (empty($this->faker))
+		{
+			$faker = Faker\Factory::create();
+			$faker->addProvider(new Faker\Provider\Base($faker));
+			$faker->addProvider(new Faker\Provider\Lorem($faker));
+		}
+		return $this->faker = $faker;
+	}
+
 	/**
 	 * Run the database seeds.
 	 *
