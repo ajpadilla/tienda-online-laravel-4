0f2b35bd2af1f1da13759f1f002d51a249e6579a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-23 22:10:46 -0430

    Actualizando clase

--- app/database/seeds/ClassifiedConditionsLangTableSeeder.php
+++ app/database/seeds/ClassifiedConditionsLangTableSeeder.php
@@ -16,7 +16,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Amueblado', 
-			'classified_types_id' =>1,
+			'classified_conditions_id' =>1,
 			'language_id' => 1,  
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')             
@@ -24,7 +24,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Furnished',
-			'classified_types_id' =>1,
+			'classified_conditions_id' =>1,
 			'language_id' => 2,
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')    
@@ -32,7 +32,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'No amueblado', 
-			'classified_types_id' =>2,
+			'classified_conditions_id' =>2,
 			'language_id' => 1,  
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')             
@@ -40,7 +40,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Not Furnished',
-			'classified_types_id' =>2,
+			'classified_conditions_id' =>2,
 			'language_id' => 2,
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')    
@@ -48,7 +48,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'No mascotas', 
-			'classified_types_id' =>3,
+			'classified_conditions_id' =>3,
 			'language_id' => 1,  
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')             
@@ -56,7 +56,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'No pets',
-			'classified_types_id' =>3,
+			'classified_conditions_id' =>3,
 			'language_id' => 2,
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')    
@@ -64,7 +64,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Servicio de Internet', 
-			'classified_types_id' =>4,
+			'classified_conditions_id' =>4,
 			'language_id' => 1,  
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')             
@@ -72,7 +72,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Internet Service',
-			'classified_types_id' =>4,
+			'classified_conditions_id' =>4,
 			'language_id' => 2,
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')    
@@ -80,7 +80,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Servicio de Teléfono', 
-			'classified_types_id' =>5,
+			'classified_conditions_id' =>5,
 			'language_id' => 1,  
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')             
@@ -88,7 +88,7 @@ class ClassifiedConditionsLangTableSeeder extends DatabaseSeeder{
 
 		$classified_conditions_lang [] = array(
 			'name' => 'Phone Service',
-			'classified_types_id' =>5,
+			'classified_conditions_id' =>5,
 			'language_id' => 2,
 			'created_at' => $date->format('Y-m-d h:m:s'),
 			'updated_at' => $date->format('Y-m-d h:m:s')    
