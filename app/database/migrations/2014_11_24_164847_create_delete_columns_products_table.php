239a11e0b567827a84edf3583a4b6df329972596
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-24 12:34:43 -0430

    Eliminando columnas name y description de la tabla productos

---
+++ app/database/migrations/2014_11_24_164847_create_delete_columns_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeleteColumnsProductsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('products', function($table)
+		{
+			$table->dropColumn('name');
+			$table->dropColumn('description');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
