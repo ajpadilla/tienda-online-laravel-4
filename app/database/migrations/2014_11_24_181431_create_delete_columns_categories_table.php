f23577ed0f5eb7a3db31c4a39329d901d3ee6566
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-24 13:50:37 -0430

    Eliminando columna name, de la tabla categories

---
+++ app/database/migrations/2014_11_24_181431_create_delete_columns_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeleteColumnsCategoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('categories', function($table)
+		{
+			$table->dropColumn('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
