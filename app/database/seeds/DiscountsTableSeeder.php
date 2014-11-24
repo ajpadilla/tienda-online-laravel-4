6518f6ee879a2fab448352196e7826cea518fe49
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-23 22:11:34 -0430

    Actualizando datos

--- app/database/seeds/DiscountsTableSeeder.php
+++ app/database/seeds/DiscountsTableSeeder.php
@@ -20,20 +20,17 @@ class DiscountsTableSeeder extends DatabaseSeeder{
 		$discountsTypes = DiscountType::all();
 
 		foreach ($discountsTypes as $discountType) {
-			// for ($i = 0; $i < rand(-1, 5); $i++)
-			 //{
-				Discount::create([
-					'value' => $faker->randomFloat(2, 17, 100),
-					'percent' =>  $faker->randomNumber(2),
-					'quantity' => $faker->numberBetween(0, 100),
-					'quantity_per_user' => $faker->numberBetween(0, 100),
-					'code' => $faker->numerify('Code###'),
-					'active' => $faker->numberBetween(0, 1),
-					'from' => $faker->dateTimeThisYear($max = 'now'),
-					'to' =>$faker->dateTimeThisYear($max = 'now'),
-					'discount_type_id' => $discountType->id,
+			Discount::create([
+				'value' => $faker->randomFloat(2, 17, 100),
+				'percent' =>  $faker->randomNumber(2),
+				'quantity' => $faker->numberBetween(0, 100),
+				'quantity_per_user' => $faker->numberBetween(0, 100),
+				'code' => $faker->numerify('Code###'),
+				'active' => $faker->numberBetween(0, 1),
+				'from' => $faker->dateTimeThisYear($max = 'now'),
+				'to' =>$faker->dateTimeThisYear($max = 'now'),
+				'discount_type_id' => $discountType->id,
 				]);
-			 //}
 		}
 	}
 
