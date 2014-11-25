531ed43542ec34fd6d708c9b0aa79eed41d1186b
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-24 22:09:14 -0430

    Agregando metodo createNewProduct

--- app/s4h/store/Products/ProductRepository.php
+++ app/s4h/store/Products/ProductRepository.php
@@ -1,6 +1,6 @@
 <?php namespace s4h\store\Products;
 
-	
+
 use s4h\store\Products\Product;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use s4h\store\Languages\Language;
@@ -22,6 +22,35 @@ class ProductRepository {
 		$product = Product::find($id);
 
 		$product->delete();
+
+	}
+
+	public function createNewProduct($data = array())
+	{
+		$product = new Product;
+		$product->name = $data['name'];
+		$product->description = $data['description'];
+		$product->on_sale = $data['on_sale'];
+		$product->quantity = $data['quantity'];
+		$product->price = $data['price'];
+		$product->measure_id = $data['measure_id'];
+		$product->width = $data['width'];
+		$product->height = $data['height'];
+		$product->depth = $data['depth'];
+		$product->weight = $data['weight'];
+		$product->active = $data['active'];
+		$product->available_for_order = $data['available_for_order'];
+		$product->show_price = $data['show_price'];
+		$product->accept_barter = $data['accept_barter'];
+		$product->product_for_barter = $data['product_for_barter'];
+		$product->condition_id = $data['condition_id'];
+
+
+		/*$user = Auth::user();
+		$product->associate($user);*/
+		$this->productRepository->save($product);
+		if (!is_null($data['categories']))
+			$product->categories()->sync($data['categories']);
 
 	}
 
