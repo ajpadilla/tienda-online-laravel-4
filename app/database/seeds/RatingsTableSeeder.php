a4777812cb30e3d13eb5dd0f8c2cfb72668284f5
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-25 16:45:34 -0430

    Agregando datos de prueba para la tabla raitings

--- app/database/seeds/RatingsTableSeeder.php
+++ app/database/seeds/RatingsTableSeeder.php
@@ -1,20 +1,45 @@
 <?php
 
 use s4h\store\Ratings\Rating;
+use s4h\store\Users\User;
+use s4h\store\Products\Product;
 
-class RatingsTableSeeder extends Seeder {
+class RatingsTableSeeder extends DatabaseSeeder {
 
 		/**
 	 * Run the database seeds.
 	 *
 	 * @return void
 	 */
-	public function run()
-	{
-		for ($i = 0; $i < 3; $i++)
+		public function run()
 		{
-			Rating::create([]);
+			$faker = $this->getFaker();
+
+			$users   = User::all()->toArray();
+
+			$products = Product::all();
+
+			foreach ($products as $product)
+			{
+
+				$used = [];
+				for ($i = 0; $i < rand(1, 5); $i++)
+				{
+
+					$user = $faker->randomElement($users);
+
+					if (!in_array($user["id"], $used))
+					{
+						$id = $user["id"];
+						Rating::create([
+							"points" => $faker->numberBetween(0, 100),
+							"user_id"   => $id,
+							"product_id" => $product->id,
+							]);
+						$used[] = $user["id"];
+					}
+				}
+			}
 		}
-	}
 
 }
