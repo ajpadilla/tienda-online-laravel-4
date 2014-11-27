221aca65b0f663c2d0c3ca267a189de4b1625e44
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-27 13:41:28 -0430

    Agregando nuevas traducciones

--- app/lang/es/invoiceStatus.php
+++ app/lang/es/invoiceStatus.php
@@ -9,14 +9,21 @@
 		"destroy"=>"eliminar/estatus/factura/{id}",
 		"title" => "Agregar estatus para factura",
 		"subtitle" => "Crear estatus para factura",
+		"sending" => "Agregando estatus para factura",
+		"response" => "Estatus para factura agregado",
+		"updated" => "Estatus para factura actualizado",
+		"delete" => "¡Estatus para factura eliminado con exito!",
+
 		"labels" => array(
 			'language' => 'Idioma',
 			'name' =>'Nombre:',
 			'description' => 'Descripción:',
 			'color' => 'Color',
 			'save' => 'Agregar',
-			'sending' => 'Enviando datos'
+			'sending' => 'Enviando datos',
+			'new' => 'Nuevo'
 		),
+
 		"validation" => array(
 			'required' => 'Campo obligatorio.',
 			'rangelength' => 'Por favor, introduzca un valor entre ',
@@ -26,10 +33,12 @@
 			'onlyLettersNumbersAndSpaces' => 'Ingrese solo letras, numeros y/o espacios en blanco',
 			'onlyLettersNumbersAndDash' => 'Ingrese solo letras, numeros y/o guiones.'
 		),
+
 		'message1' =>'Estatus para factura',
 		'message2' =>'Agregada con exito!.',
 		'alert' => '¡El nombre del estatus para la factura ya se encuentra registrado!',
 		'alertColor' => '¡El color ya se encuentra registrado!.',
+
 		'list' =>array(
 			'title' => 'Listado de facturas',
 			'subtitle'=>'Lista de facturas',
@@ -38,4 +47,20 @@
 			'Description' => 'Descripción',
 			'Actions'=>'Acciones',
 		),
+
+		'edit_view' => array(
+			'title' => 'Editar Estatus para factura',
+			'subtitle' => 'Datos del estatus para factura'
+		),
+
+		'show_data' => array(
+			'title' => 'Ver datos del estatus para factura',
+			'subtitle' => 'Estatus para factura'
+		),
+
+		'actions' => array(
+			'Show' => 'Ver   ',
+			'Edit' => 'Editar',
+			'Delete' => 'Eliminar'
+		)
 	);
