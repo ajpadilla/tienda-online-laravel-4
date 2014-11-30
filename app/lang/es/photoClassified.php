3f2bfe6aff309b3b4e82f8a5adf6aff8e984cf1a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-29 21:10:36 -0430

    Agregando taducciones para el modelo classified_photos

---
+++ app/lang/es/photoClassified.php
@@ -0,0 +1,33 @@
+<?php 
+	return array(
+		"create" => "crear/fotoClasificado",
+		"store" => "agregar/fotoClasificado",
+		"title" => "Agregar foto a clasificado",
+		"subtitle" => "Adjuntar fotos al clasificado",
+
+		"labels" => array(
+			'add' => 'Agregar fotos...',
+			'init' => 'Iniciar',
+			'cancel' => 'Cancelar',
+			'delete' => 'Eliminar'
+ 		),
+		"list" => array(
+			'title' => 'Lista de clasificados',
+			'Name' => 'Nombre',
+			'Actions' =>'Acciones'
+		),
+		"validation" => array(
+			'required' => 'Campo obligatorio.',
+			'rangelength' => 'Por favor, introduzca un valor entre ',
+			'characters' => 'caracteres.',
+			'number' => 'Por favor, introduzca un número válido.',
+			'digits' => 'Por favor, ingrese sólo dígitos enteros.',
+			'date' => 'Por favor, Ingrese una fecha valida.',
+			'onlyLettersNumbersAndSpaces' => 'Ingrese solo letras, numeros y espacios en blanco',
+			'onlyLettersNumbersAndDash' => 'Ingrese solo letras, numeros y/o guiones.'
+		),
+		'message1' =>'Nuevo clasificado',
+		'message2' =>'Agregado con exito!.',
+		'alert' => '¡El nombre ya se encuentra registrado!',
+		'sending' => 'Enviando datos'
+	);
