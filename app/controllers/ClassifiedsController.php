441352facc605cec008f7cbb9a8149b367f3ae13
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-03 22:55:06 -0430

    Modificando el nro de fotos a mostrar por clasificado

--- app/controllers/ClassifiedsController.php
+++ app/controllers/ClassifiedsController.php
@@ -44,11 +44,23 @@ class ClassifiedsController extends \BaseController {
 		$collection->addColumn('photo', function($model)
 		{
 			$links = '';
-			if($model->classified->hasPhotos()){
+			/*if($model->classified->hasPhotos()){
 					$photo = $model->classified->getFirstPhoto();
 					$links = "<a href='#'><img class='mini-photo' alt='" . $photo->filename . "' src='" . asset($photo->complete_path) . "'></a>";
+			}*/
 
+			$i = 0;
+			foreach ($model->classified->photos as $photo) {
+				if ($i < 3) {
+					$links .= "	<a href='#'>
+									<img class='mini-photo' alt='" . $photo->filename . "' src='" . asset($photo->complete_path) . "'>
+								</a>";
+				} else {
+					break;
+				}
+				$i++;
 			}
+
 			return $links;
 		});
 
