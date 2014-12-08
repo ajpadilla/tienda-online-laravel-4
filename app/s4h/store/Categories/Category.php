6f3d04edae6f4a9bdeb94d803d5e1650d9916087
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-08 13:01:39 -0430

    Agregando metodo categories y category, para relacionar las categorias por clasificacion

--- app/s4h/store/Categories/Category.php
+++ app/s4h/store/Categories/Category.php
@@ -6,10 +6,25 @@ use Illuminate\Database\Eloquent\SoftDeletingTrait;
 class Category extends Eloquent{
 
 	use SoftDeletingTrait;
+
 	protected $softDelete = true;
+
 	protected $dates = ['deleted_at'];
+
+	protected $table = 'categories';
 
 	public function languages(){
 		return $this->belongsToMany('s4h\store\Languages\Language','categories_lang','categories_id','language_id')->withPivot('name');
 	}	
+
+	public function categories()
+	{
+		return $this->hasMany('s4h\store\Categories\Category');
+	}
+
+	public function category()
+	{
+		return $this->belongsTo('s4h\store\Categories\Category','category_id');
+	}
+	
 }
