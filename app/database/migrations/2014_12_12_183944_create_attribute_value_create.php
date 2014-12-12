f7d232e947df00133aef59ff15e003a9270a0f48
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-12 14:22:02 -0430

    Agreagando tabla para almacenar los valores de cada atributo para cada producto

---
+++ app/database/migrations/2014_12_12_183944_create_attribute_value_create.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeValueCreate extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('attributes_values', function(Blueprint $table)
+		{
+			$table->increments('id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('attributes_values');
+	}
+
+}
