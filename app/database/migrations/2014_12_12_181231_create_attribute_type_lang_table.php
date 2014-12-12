c65d63bdaa83f79d30cbb0c7b8d39e96eab14bc4
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-12 13:47:14 -0430

    Agregando migracion para la tabla attribute_types_lang

---
+++ app/database/migrations/2014_12_12_181231_create_attribute_type_lang_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeTypeLangTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('attribute_types_lang', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 255);
+			$table->integer('attribute_type_id')->index();
+			$table->integer('language_id')->index();
+			$table->unique(array('attribute_type_id', 'language_id'), 'attribute_types_lang_unique');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('attribute_types_lang');
+	}
+
+}
