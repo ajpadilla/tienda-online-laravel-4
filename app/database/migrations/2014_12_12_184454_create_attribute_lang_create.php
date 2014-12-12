fc2da6d0edcf3e9d2e434909be7d7f2f1167fc3a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-12 14:22:55 -0430

    Agreagando tabla para almacenar los valores de la traduccion para cada atributo

---
+++ app/database/migrations/2014_12_12_184454_create_attribute_lang_create.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeLangCreate extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('attributes_lang', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 255);
+			$table->integer('attribute_id')->index();
+			$table->integer('language_id')->index();
+			$table->unique(array('attribute_id', 'language_id'), 'attributes_lang_unique');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('attributes_lang');
+	}
+
+}
