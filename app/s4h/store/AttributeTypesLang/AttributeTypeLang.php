f2c4231c41f379676bf4c1162d04bb3eba1a33d9
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-13 08:22:22 -0430

    Agregando relacion con los modelos languages y attribute_types

--- app/s4h/store/AttributeTypesLang/AttributeTypeLang.php
+++ app/s4h/store/AttributeTypesLang/AttributeTypeLang.php
@@ -17,4 +17,12 @@ class AttributeTypeLang extends Eloquent
 
 	protected $table = 'attribute_types_lang';
 
+	public function attributeType(){
+		return $this->belongsTo('s4h\store\AttributeTypes\AttributeType','attribute_type_id');
+	}
+
+	public function language(){
+		return $this->belongsTo('s4h\store\Languages\Language','language_id');
+	}
+
 }
