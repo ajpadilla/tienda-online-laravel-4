5bb472e092cd0ac25caa66341c4a528beb60d8eb
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-13 08:00:42 -0430

    Agregando archivo para las traducciones del modelo attribute_types

---
+++ app/lang/es/attributeType.php
@@ -0,0 +1,60 @@
+<?php 
+return array(
+	"create" => "crear/tipo/atributo",
+	"store" => "agregar/tipo/atributo",
+	"index" => "lista/tipo/atributo",
+	"show"=>"ver/tipo/atributo/{id}",
+	"edit"=>"editar/tipo/atributo/{id}",
+	"update"=>"actualizar/tipo/atributo/{id}",
+	"destroy"=>"eliminar/tipo/atributo/{id}",
+	"title" => "Agregar tipo/atributo",
+	"subtitle" => "Crear tipo/atributo",
+	"sending" => "Agregando tipo/atributo",
+	"response" => "Atributo agregada",
+	"Actualiced" => "Atributo actualizada",
+	"Delete" => "Atributo eliminada con exito!",
+
+	"labels" => array(
+		'language' => 'Idioma',
+		'name' =>'Nombre:',
+		'save' => 'Agregar',
+		'new' => 'Nuevo'
+	),
+
+	"list" => array(
+		'title' => 'Lista de atributos',
+		'subtitle' => 'Listado de atributos',
+		'Name' => 'Nombre',
+		'Actions' =>'Acciones'
+	),
+
+	"validation" => array(
+		'required' => 'Campo obligatorio.',
+		'rangelength' => 'Por favor, introduzca un valor entre ',
+		'characters' => 'caracteres.',
+		'number' => 'Por favor, introduzca un número válido.',
+		'digits' => 'Por favor, ingrese sólo dígitos enteros.',
+		'date' => 'Por favor, Ingrese una fecha valida.',
+		'onlyLettersNumbersAndSpaces' => 'Ingrese solo letras, numeros y espacios en blanco',
+		'onlyLettersNumbersAndDash' => 'Ingrese solo letras, numeros y/o guiones.'
+	),
+
+	'alert' => '¡El nombre ya se encuentra registrado!',
+
+	'edit_view' => array(
+		'title' => 'Editar atributo',
+		'subtitle' => 'Datos atributo'
+	),
+
+	'show_data' => array(
+		'title' => 'Ver datos de la atributo',
+		'subtitle' => 'Datos de la atributo'
+	),
+
+	'actions' => array(
+		'Show' => 'Ver   ',
+		'Edit' => 'Editar',
+		'Delete' => 'Eliminar'
+	)
+
+);
