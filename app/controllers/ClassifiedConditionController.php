1bc8e8cc1d3d78ebd4aa9428397ec8b7ee1ce9cd
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-16 10:47:03 -0430

    Modificando estilo de botones en el metodo datatable

--- app/controllers/ClassifiedConditionController.php
+++ app/controllers/ClassifiedConditionController.php
@@ -50,11 +50,11 @@ class ClassifiedConditionController extends \BaseController {
 	
 		$collection->addColumn('Actions',function($model){
 		
-			$links = "<a class='btn btn-info' href='" . route('classifiedConditions.show', $model->classifiedCondition->id) . "'>".trans('classifiedConditions.actions.Show')." <i class='fa fa-check'></i></a>
+			$links = "<a class='btn btn-info btn-circle' href='" . route('classifiedConditions.show', $model->classifiedCondition->id) . "'><i class='fa fa-check'></i></a>
 					<br />";
-			$links .= "<a a class='btn btn-warning' href='" . route('classifiedConditions.edit', $model->classifiedCondition->id) . "'>".trans('classifiedConditions.actions.Edit')." <i class='fa fa-pencil'></i></a>
+			$links .= "<a a class='btn btn-warning btn-circle' href='" . route('classifiedConditions.edit', $model->classifiedCondition->id) . "'><i class='fa fa-pencil'></i></a>
 					<br />
-					<a class='btn btn-danger' href='" . route('classifiedConditions.destroy', $model->classifiedCondition->id) . "'>".trans('classifiedConditions.actions.Delete')." <i class='fa fa-times'></i></a>";
+					<a class='btn btn-danger btn-circle' href='" . route('classifiedConditions.destroy', $model->classifiedCondition->id) . "'><i class='fa fa-times'></i></a>";
 
 			return $links;
 		});
@@ -183,5 +183,20 @@ class ClassifiedConditionController extends \BaseController {
 		}
 		return Response::json(array('respuesta' => 'false'));
 	}
+
+	public function checkNameForEdit()
+	{
+		$response = array();
+		if (Request::ajax()) {
+			$classified_condition = $this->classifiedConditionsRepository->getNameForEdit(Input::all());
+			if (count($classified_condition) > 0) {
+				return Response::json(false);
+			} else {
+				return Response::json(true);
+			}
+		}
+		return Response::json(array('response' => 'false'));
+	}
+
 
 }
