bc49495fd6dd1e137e687a9efb7339f537bc5579
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-19 17:37:33 -0430

    Actualizando datos de prueba, para el modelo

--- app/database/seeds/ProductTableSeeder.php
+++ app/database/seeds/ProductTableSeeder.php
@@ -35,16 +35,13 @@ class ProductTableSeeder extends DatabaseSeeder{
 				'quantity' => $faker->numberBetween(0, 100),
 				'price' => $faker->randomFloat(20, 6, 100),
 				'width' => $faker->randomFloat(2, 6, 100),
+				'point_price' => $faker->randomFloat(20, 6, 100),
 				'height' => $faker->randomFloat(2, 6, 100),
 				'depth' => $faker->randomFloat(2, 6, 100),
 				'weight' => $faker->randomFloat(2, 6, 100),
 				'active' => $faker->numberBetween(0,1),
-				'available_for_order' => $faker->numberBetween(0,1),
-				'show_price' => $faker->numberBetween(0,1),
 				'accept_barter' => $faker->numberBetween(0,1),
-				'product_for_barter' => $faker->numberBetween(0,1),
 				'condition_id' => $productCondition['id'],
-				'measure_id' => $measur['id'],
 				'user_id' => $user['id'],
 			]);
 		}
