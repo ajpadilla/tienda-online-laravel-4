4cb65a02ea250da8ee90b74ec112e25ca896b2bf
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-27 14:33:07 -0430

    Agregando resultados de la busqueda para los clasificado

--- app/views/products/search.blade.php
+++ app/views/products/search.blade.php
@@ -15,34 +15,64 @@
 			@include('flash::message')
 			<div class="ibox-content">
 				@if (count($productResults) > 0)
-				@foreach ($productResults as $category => $products)
-				{{$category}}
-				<table class="row-border dataTable no-footer" cellspacing="0" width="100%">
-					<thead>
-						<tr role="row">
-							<th>Puntos</th>
-							<th>Nombre</th>
-							<th>Categorias</th>
-							<th>Precio</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($products as $key => $productSearch)
-						<tr role="row">
-							<!--<td>{{ $key }}</td>-->
-							<td>{{ $productSearch->product->point_price }}</td>	
-							<td>{{ $productSearch->name }}</td>	
-							<td>
-								@foreach ($productSearch->product->getCategories($language_id) as $value)
-								{{ $value }}
-								@endforeach
-							</td>	
-							<td>{{ $productSearch->product->price }}</td>	
-						</tr>
-						@endforeach
-					</tbody>
-				</table>
-				@endforeach
+					@foreach ($productResults as $category => $products)
+					{{$category}}
+					<table class="row-border dataTable no-footer" cellspacing="0" width="100%">
+						<thead>
+							<tr role="row">
+								<th>Puntos</th>
+								<th>Nombre</th>
+								<th>Categorias</th>
+								<th>Precio</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($products as $key => $productSearch)
+							<tr role="row">
+								<!--<td>{{ $key }}</td>-->
+								<td>{{ $productSearch->product->point_price }}</td>	
+								<td>{{ $productSearch->name }}</td>	
+								<td>
+									@foreach ($productSearch->product->getCategories($language_id) as $value)
+									{{ $value }}
+									@endforeach
+								</td>	
+								<td>{{ $productSearch->product->price }}</td>	
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+					@endforeach
+				@endif
+				@if (count($categoryResults) > 0)
+					@foreach ($categoryResults as $category => $classifieds)
+					{{$category}}
+					<table class="row-border dataTable no-footer" cellspacing="0" width="100%">
+						<thead>
+							<tr role="row">
+								<!--<th>Puntos</th>-->
+								<th>Nombre</th>
+								<th>Categorias</th>
+								<th>Precio</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($classifieds as $key => $classifiedSearch)
+							<tr role="row">
+								<!--<td>{{ $key }}</td>-->
+								<!--<td>{{ $classifiedSearch->classified->price }}</td>-->
+								<td>{{ $classifiedSearch->name }}</td>	
+								<td>
+									@foreach ($classifiedSearch->product->getCategories($language_id) as $value)
+									{{ $value }}
+									@endforeach
+								</td>	
+								<td>{{ $classifiedSearch->classified->price }}</td>	
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+					@endforeach
 				@endif
 			</div>
 		</div>
