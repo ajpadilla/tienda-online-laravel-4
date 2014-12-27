1880f14b7559c0e94ec15b338641ef9d90ef6978
Alvaro Padilla <ajpadilla88@gmail.com>
2014-12-27 14:43:02 -0430

    Agregando datos de prueba para la tabla ClassifiedClassificationTableSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -61,6 +61,7 @@ class DatabaseSeeder extends Seeder {
 	     $this->call('AttributeValueTableSeeder');
 	     $this->call('AttributeValueLangTableSeeder');
 	     $this->call('AttributeByValueTableSeeder');
+	     $this->call('ClassifiedClassificationTableSeeder');
 	}
 
 }
