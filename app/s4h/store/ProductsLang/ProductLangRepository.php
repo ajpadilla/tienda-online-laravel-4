86f18f50509bdfa660e0f018342fca6bb6ecf1c5
Lenyn Alcantara <nightzpy@gmail.com>
2015-01-10 03:11:32 +0000

    Agregadas fotos para cada producto en la pagina principal

--- app/s4h/store/ProductsLang/ProductLangRepository.php
+++ app/s4h/store/ProductsLang/ProductLangRepository.php
@@ -3,9 +3,10 @@
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use s4h\store\Languages\Language;
 use s4h\store\ProductsLang\ProductLang;
+use s4h\store\Products\Product;
 
   /**
-  * 
+  *
   */
   class ProductLangRepository {
 
@@ -15,9 +16,10 @@ use s4h\store\ProductsLang\ProductLang;
   	}
 
 
-	public function getNewProducts($quantity = 4){
-		$isoCode = LaravelLocalization::setLocale();
-		$language = Language::select()->where('iso_code','=',$isoCode)->first();
-		return ProductLang::whereLanguageId($language->id)->orderBy('created_at', 'DESC')->take($quantity)->get();
-	}
-  }	
+  	public function getNewProducts($quantity = 4){
+  		$isoCode = LaravelLocalization::setLocale();
+  		$language = Language::select()->where('iso_code','=',$isoCode)->first();
+      $products = Product::whereActive(TRUE)->orderBy('created_at', 'DESC')->take($quantity)->lists('id');
+  		return ProductLang::with('product')->whereIn('product_id', $products)->whereLanguageId($language->id)->get();
+  	}
+  }
--- app/views/products/partials/_new-products.blade.php
+++ app/views/products/partials/_new-products.blade.php
@@ -3,10 +3,17 @@
 		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
 		    <div class="product-item">
 		        <div class="pi-img-wrapper">
-		            <img class="img-responsive" src="{{ asset('uploads/products/images/model1.jpg') }}" alt=""/>
-		            <div>
-		              <a href="{{ asset('uploads/products/images/model1.jpg') }}" class="btn btn-default fancybox-button"><i class="fa fa-search-plus fa-3x"></i>
-		</a>
+					@if($product->product->hasPhotos())
+			            <img class="img-responsive" src="{{ asset($product->product->getFirstPhoto()->complete_path) }}" alt="{{ $product->product->getFirstPhoto()->filename }}"/>
+			            <div>
+			              <a href="{{ asset($product->product->getFirstPhoto()->complete_path) }}" class="btn btn-default fancybox-button" alt="{{ $product->product->getFirstPhoto()->filename }}"><i class="fa fa-search-plus fa-3x"></i>
+			</a>
+					@else
+			            <img class="img-responsive" src="{{ asset('/uploads/products/images/model1.jpg') }}" alt="not-path"/>
+			            <div>
+			              <a href="{{ asset('/uploads/products/images/model1.jpg') }}" class="btn btn-default fancybox-button" alt="not-path"><i class="fa fa-search-plus fa-3x"></i>
+			</a>
+					@endif
 		              <a href="#product-pop-up-{{ $product->product_id }}" class="btn btn-default fancybox-fast-view"><i class="fa fa-eye fa-3x"></i></a>
 		            </div>
 		        </div>
