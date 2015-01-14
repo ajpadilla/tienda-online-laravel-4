0c5983047fa2d27b53607dcd2ec354bc50372f5e
Alvaro Padilla <ajpadilla88@gmail.com>
2015-01-13 21:23:57 -0430

    Actualizando vistar, para mostrar los resultados de la busqueda de los clasificados

--- app/views/classifieds/filtered-classisied.blade.php
+++ app/views/classifieds/filtered-classisied.blade.php
@@ -1,11 +1,39 @@
 @extends('layouts.template')
 
 @section('title')
-{{ trans('products.list.title') }}
+{{ trans('classifieds.filtered.title') }}
 @stop
 
 @section('content')
-
+@include('layouts.partials._error')
+<div class="row">
+	<div class="col-lg-12">
+		<div class="ibox float-e-margins">
+			<div class="ibox-title">
+				<h5>{{trans('classifieds.filtered.subtitle')}}</h5>
+			</div>
+			@include('flash::message')
+			<div class="ibox-content">
+				@if (!empty($classifiedsResult))
+				@foreach ($classifiedsResult as $classified)
+				<table class="row-border dataTable no-footer" cellspacing="0" width="100%">
+					<thead>
+						<tr role="row">
+							<th>Nombre</th>
+							<th>Precio</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr role="row">
+							<td>{{ $classified->getDataForLanguage($languageId)->pivot->name }}</td>	
+							<td>{{ $classified->price }}</td>	
+						</tr>
+					</tbody>
+				</table>
+				@endforeach
+				@endif
+			</div>
+		</div>
+	</div>
+</div>
 @stop
-
-
