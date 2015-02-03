d380af4325bc38e22dcf6ec2080c60e786be6e3e
Lenyn Alcantara <nightzpy@gmail.com>
2015-02-03 01:55:02 +0000

    Accion para agregar productos a la lista de deseos

---
+++ app/controllers/WishlistController.php
@@ -0,0 +1,90 @@
+<?php
+
+class WishlistController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create($id)
+	{
+		$response = ['success' => FALSE];
+		if(Auth::user() && (Auth::user()->isAdminClient() || Auth::user()->isReadClient()))
+			if($this->productRepository->addToUserWishList($id, Auth::user()))
+				$response['success'] => TRUE;
+		return $response;
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
