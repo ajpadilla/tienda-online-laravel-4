af00af5107257311f981ddbef0911368402b438d
Lenyn Alcantara <nightzpy@gmail.com>
2015-02-03 09:25:59 +0000

    Agregados roles para los usuarios y permiso para agregar a lista de deseos

--- app/models/User.php
+++ app/models/User.php
@@ -1,19 +1,22 @@
 <?php
 
 use Zizaco\Confide\ConfideUser;
+use Zizaco\Entrust\HasRole;
 
 class User extends ConfideUser {
+
+    use HasRole;
 
     public function userdetail()
     {
         return $this->hasOne('Userdetail');
     }
-	
+
 	public function familie()
     {
         return $this->hasOne('Familie');
     }
-	
+
     public function calendars()
     {
         return $this->hasMany('Calendar');
