cbd5355345f003f2626b0367a30be3365ff69d38
ajpadilla <ajpadilla88@gmail.com>
2015-02-05 14:15:26 -0430

    agregando blade

---
+++ app/views/products/partials/_form_language.blade.php
@@ -0,0 +1,48 @@
+<div class="col-lg-7">
+
+	<div class="form-group">
+		{{ Form::label('language_id', trans('products.labels.language'),['class'=>'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			{{ Form::select('language_id',$languages,null,['class' => 'form-control','id'=>'language_id']) }}
+		</div>
+	</div>
+
+	<div class="form-group" style="display: none">
+		{{ Form::label('product_id','id', ['class' => 'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			{{ Form::text('product_id', null, ['class' => 'form-control', 'placeholder' =>'','id'=> 'product_id']) }}
+		</div>
+	</div>
+
+
+	<div class="form-group">
+		{{ Form::label('name',  trans('products.labels.name') , ['class' => 'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			{{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name', 'id' => 'name']) }}
+		</div>
+	</div>
+
+	<div class="form-group">
+		{{ Form::label('description', trans('products.labels.description'), ['class' => 'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			<div class="ibox-content no-padding">
+				{{ Form::textarea('description', null, ['class' => 'form-control', 'id' => 'description']) }}
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<div class="col-lg-6 col-lg-offset-2">
+	<div class="form-group">
+		<div class="col-sm-3">
+			<button type="submit" class="pull-right btn btn-primary" name="add_photos" value="0">Guardar</button>
+		</div>
+		<div class="col-sm-3">
+			<button type="submit" class="pull-right btn btn-primary" name="add_photos" value="1">{{ trans('products.labels.add_photos') }}</button>
+		</div>
+	</div>
+</div>
+
+<div class="clearfix"></div>
+
