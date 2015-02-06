96bb8dc8cca1af3c3d8bac279505896d8ea34e68
ajpadilla <ajpadilla88@gmail.com>
2015-02-06 19:00:23 -0430

    Agregando datos de prueba al modelo carts

---
+++ app/database/seeds/CartsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php 
+
+use s4h\store\Currencies\Currency;
+use s4h\store\Carts\Cart;
+use s4h\store\Users\User;
+
+/**
+* 
+*/
+class CartsTableSeeder extends DatabaseSeeder{
+	
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+        $faker = $this->getFaker();
+
+        $currencies = Currency::all()->toArray();
+
+        $users = User::all()->toArray();
+
+        $used = [];
+
+        for ($i=0; $i < rand(1, 8); $i++) 
+        { 
+
+            $currency = $faker->randomElement($currencies);
+
+            $user = $faker->randomElement($users);
+
+            if (!in_array($user["id"], $used))
+            {
+                Cart::create([
+                    'user_id' => $user['id'],
+                    'currency_id' => $currency['id']
+                ]);
+                $used[] = $user["id"];
+            }
+        }
+	}
+
+}
