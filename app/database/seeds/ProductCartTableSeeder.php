119029ae1ea92b28dfb4e8e994cd4d5c833a85b6
ajpadilla <ajpadilla88@gmail.com>
2015-02-06 19:01:02 -0430

    Agregando datos de prueba al modelo product_cart

---
+++ app/database/seeds/ProductCartTableSeeder.php
@@ -0,0 +1,49 @@
+<?php 
+
+
+use s4h\store\Products\Product;
+use s4h\store\Carts\Cart;
+use s4h\store\ProductsCarts\ProductCart;
+
+/**
+* 
+*/
+class ProductCartTableSeeder extends DatabaseSeeder{
+	
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+        $faker = $this->getFaker();
+
+        $products = Product::all()->toArray();
+
+        $carts = Cart::all();
+
+        $used = [];
+
+        foreach ($carts as $cart)
+        {
+
+          $used = [];
+          for ($i = 0; $i < rand(1, 3); $i++)
+          {
+
+            $product = $faker->randomElement($products);
+
+            if (!in_array($product["id"], $used))
+            {
+              ProductCart::create([
+                "product_id"   => $product["id"],
+                "cart_id" => $cart->id,
+                ]);
+              $used[] = $product["id"];
+          }
+      }
+  }
+}
+
+}
