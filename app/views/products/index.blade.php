fafdb5f22c634a23cc22c3f9fb2869ab207d7e7b
ajpadilla <ajpadilla88@gmail.com>
2015-02-07 17:14:40 -0430

    Actualizando blade

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -66,7 +66,7 @@
 			<div class="col-lg-12">
 				<div class="ibox float-e-margins">
 					<div class="ibox-title">
-						<h5>{{	trans('products.subtitle') }}</h5>
+						<h5>{{	trans('products.edit_language.title') }}</h5>
 					</div>
 					<div class="ibox-content">
 						<div class="row">
