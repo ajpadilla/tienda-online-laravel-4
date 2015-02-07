6b8abd8b55d690a3f13abd8331e253aff73ad329
ajpadilla <ajpadilla88@gmail.com>
2015-02-07 18:37:03 -0430

    Agregando seccion para los clasificados

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -35,7 +35,13 @@
 			    </div>
 
 			    <div id="clasificados" class="active">
+			    	<h2>{{ Lang::get('classifieds.labels.NewClassifieds') }}</h2>
+				    @include('products.partials._new-classifieds')
 			    </div>
+			    <div class="row">
+				        <h2>{{ Lang::get('classifieds.labels.TopClassifieds') }}</h2>
+				        @include('products.partials._top-products')
+			        </div>
 		    </div>
 	    </div>
     </div>
