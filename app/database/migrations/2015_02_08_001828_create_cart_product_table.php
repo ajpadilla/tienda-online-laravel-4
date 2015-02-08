2527df51943e3bedd80e0c8c42c393ea357b6a4a
Lenyn Alcantara <nightzpy@gmail.com>
2015-02-08 00:22:50 +0000

    Agregadas migraciones para el proceso del carro de compras

---
+++ app/database/migrations/2015_02_08_001828_create_cart_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCartProductTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cart_product', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('cart_id');
+			$table->integer('product_id');
+			$table->integer('quantity')->default(1);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cart_product');
+	}
+
+}
---
+++ app/database/migrations/2015_02_08_002153_create_currencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCurrenciesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('currencies', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 128);
+			$table->string('iso_code', 32);
+			$table->string('sign', 8);
+			$table->boolean('active')->default(TRUE);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('currencies');
+	}
+
+}
