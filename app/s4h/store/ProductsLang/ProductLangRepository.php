085139e488d7b4c758f54aa08bc7ec5b7ca1de21
ajpadilla <ajpadilla88@gmail.com>
2015-02-11 17:21:55 -0430

    Heredando de clase BaseRepository

--- app/s4h/store/ProductsLang/ProductLangRepository.php
+++ app/s4h/store/ProductsLang/ProductLangRepository.php
@@ -4,17 +4,23 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use s4h\store\Languages\Language;
 use s4h\store\ProductsLang\ProductLang;
 use s4h\store\Products\Product;
-
+use s4h\store\Base\BaseRepository;
   /**
   *
   */
-  class ProductLangRepository {
+  class ProductLangRepository extends BaseRepository{
 
   	public function getAllForLanguage($language_id)
   	{
   		return ProductLang::where('language_id','=',$language_id)->get();
   	}
 
+    public function getModel()
+    {
+      return new ProductLang;
+    }
+
+    public $filters = ['filterWord'];
 
     public function getProductForLanguage($productId, $languageId)
     {
