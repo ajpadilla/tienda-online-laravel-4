6322ac6a81647b6390fe77a781a6f637bad32c19
ajpadilla <ajpadilla88@gmail.com>
2015-02-14 18:54:37 -0430

    Modificando vista

--- app/views/classifieds/search-classified.blade.php
+++ app/views/classifieds/search-classified.blade.php
@@ -15,71 +15,65 @@
 			<div class="ibox-content">
 				<div class="row">
 					{{ Form::open(['route' => 'classifieds.filterClassified','class'=>'form-horizontal','id' => 'formSearchClassified']) }}
-					<div class="col-sm-6 b-r">
-						<div class="form-group">
-							{{ Form::label('countryId', trans('classifieds.searchs.Country'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('countryId',$country,null,array('class' => 'form-control','id'=>'countryId')) }}
-							</div>
-						</div>
-						<div class="form-group">
-							{{ Form::label('stateId', trans('classifieds.searchs.State'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('stateId',array(''),null,array('class' => 'form-control','id'=>'stateId')) }}
-							</div>
-						</div>
-						<div class="form-group">
-							{{ Form::label('cityId', trans('classifieds.searchs.City'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('cityId',array(''),null,array('class' => 'form-control','id'=>'cityId')) }}
-							</div>
-						</div>
-						
-						<div class="form-group">
-							{{ Form::label('price', trans('classifieds.labels.price'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::text('price',null, ['class' => 'form-control']) }}
-							</div>
-						</div>
-	
-						<div class="form-group">
-							{{ Form::label('operator', trans('classifieds.labels.classified_type'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('operator',
-									array('>' => '>', 
-										  '<' => '<',
-										  '>=' => '>=',
-										  '<=' => '<=',
-										  '==' => '=='
-										,),
-									null,array('class' => 'form-control','id'=>'operator')) }}
-							</div>
-						</div>
+						<div class="col-lg-12">
+							<div class="row">
+								<div class="form-group">
+									<div class="col-md-2">
+										<!--{{ Form::label('countryId', trans('classifieds.searchs.Country'),['class'=>'col-sm-2 control-label']) }}>-->
 
-						<div class="form-group">
-							{{ Form::label('classifiedTypeId', trans('classifieds.labels.classified_type'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('classifiedTypeId',$classifiedTypes,null,array('class' => 'form-control','id'=>'classifiedTypeId')) }}
-							</div>
-						</div>
+										 {{ Form::select('countryId',$country,null,array('class' => 'form-control','id'=>'countryId')) }}
+									</div class="col-md-2">
+											
+									<div class="col-md-2">
+										<!--{{ Form::label('stateId', trans('classifieds.searchs.State'),['class'=>'col-sm-1 control-label'])
+											}}-->
+										{{ Form::select('stateId',array(''),null,array('class' => 'form-control','id'=>'stateId')) }}
+									</div>
 
-						<div class="form-group">
-							{{ Form::label('classifiedConditionId', trans('classifieds.labels.classified_condition'),['class'=>'col-sm-2 control-label']) }}
-							<div class="col-sm-6">
-								{{ Form::select('classifiedConditionId',$classifiedConditions,null,array('class' => 'form-control','id'=>'classifiedConditionId')) }}
-							</div>
-						</div>
+									<div class="col-md-2">
+										<!--{{ Form::label('cityId', trans('classifieds.searchs.City'),['class'=>'col-sm-1 control-label']) }}-->
+										  		{{ Form::select('cityId',array(''),null,array('class' => 'form-control','id'=>'cityId')) }}
+									</div>
+												
+									<div class="col-md-2">
+										<!--{{ Form::label('price', trans('classifieds.labels.price'),['class'=>'col-sm-2 control-label']) }}-->
+										{{ Form::text('price',null, ['class' => 'form-control']) }}
+											
+									</div>
 
-					</div>
+									<div class="col-md-2">
+										<!--{{ Form::label('operator', trans('classifieds.labels.precio'),['class'=>'col-md-2 control-label']) }}-->
+											{{ Form::select('operator',
+												array('>' => '>', 
+													'<' => '<',
+													'>=' => '>=',
+													'<=' => '<=',
+													'==' => '=='
+													,),
+													null,array('class' => 'form-control','id'=>'operator')) }}
+									</div>
+											
+									<div class="col-md-2">
+										<!--{{ Form::label('classifiedTypeId', trans('classifieds.labels.classified_type'),['class'=>'col-sm-2 control-label']) }}-->
 
-					<div class="col-sm-6">
-						<div class="form-group">
-							<div class="col-sm-4 col-sm-offset-2">
-								{{ Form::submit(trans('classifieds.searchs.subtitle'), ['class' => 'btn btn-primary']) }}
+										{{ Form::select('classifiedTypeId',$classifiedTypes,null,array('class' => 'form-control','id'=>'classifiedTypeId')) }}
+									</div>
+								</div>
+								<div class="row">
+									
+									<div class="col-md-2">
+										<!--{{ Form::label('classifiedConditionId', trans('classifieds.labels.classified_condition'),['class'=>'col-sm-2 control-label']) }}-->
+									
+										{{ Form::select('classifiedConditionId',$classifiedConditions,null,array('class' => 'form-control','id'=>'classifiedConditionId')) }}
+									</div>
+									
+									<div class="col-md-4 col-lg-offset-1">
+										{{ Form::submit(trans('classifieds.searchs.subtitle'), ['class' => 'btn btn-primary']) }}
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
 					{{ Form::close() }}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -102,7 +96,7 @@
 						console.log(response.states);
 						if (response.success == true) {
 							$('#stateId').html('');
-							$('#stateId').append('<option value=\"\"> {{ trans('classifieds.searchs.State') }} </option>');
+							$('#stateId').append('<option value=\"\"> </option>');
 							$.each(response.states,function (k,v){
 								$('#stateId').append('<option value=\"'+k+'\">'+v+'</option>');
 							});
@@ -125,7 +119,7 @@
 						console.log(response.cities);
 						if (response.success == true) {
 							$('#cityId').html('');
-							$('#cityId').append('<option value=\"\"> {{ trans('classifieds.searchs.City') }} </option>');
+							$('#cityId').append('<option value=\"\"> </option>');
 							$.each(response.cities,function (k,v){
 								$('#cityId').append('<option value=\"'+k+'\">'+v+'</option>');
 							});
