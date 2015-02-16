fd2fd60a393032161d4512e0e90e58ffc543a75b
ajpadilla <ajpadilla88@gmail.com>
2015-02-16 17:00:24 -0430

    Agragando traducciones

--- app/lang/en/cart.php
+++ app/lang/en/cart.php
@@ -4,6 +4,25 @@
 		"show-title" => "Show Cart",
 		"create" => "cart/add/{id}/{quantity?}",
 		'delete-ajax' => 'cart/delete-ajax/{id}',
-		"store" => "cart/store"
+		"store" => "cart/store",
+		"cart-empty" => "Your shopping cart is empty!",
+		"title" => "Cart",
+
+		"labels" => array(
+			'name' =>'Nombre',
+			'description' => 'Description',
+			'quantity' => 'Quantity',
+			'price' => 'Price',
+			'save' => 'Save',
+			'image' => 'No images found',
+			'Image' => 'Image',
+			'Stock' => 'Stock',
+			'UnitPrice' => 'Unit price',
+			'sub-total' => "Sub total",
+			'total' => "Total",
+			'cost' => "Shipping Cost",
+			'continue-shopping' => "continue shopping",
+			'pay' => 'Pay'
+		),
 	);
 
--- app/lang/es/cart.php
+++ app/lang/es/cart.php
@@ -4,6 +4,25 @@
 		"show-title" => "Ver Carrito",
 		"create" => "carrito/agregar/{id}/{quantity?}",
 		'delete-ajax' => 'carrito/eliminar-ajax/{id}',
-		"store" => "carrito/guardar"
+		"store" => "carrito/guardar",
+		"cart-empty" => "¿Tu carro de compras está vacío!",
+		"title" => "Carrito",
+
+		"labels" => array(
+			'name' =>'Nombre',
+			'description' => 'Descripción',
+			'quantity' => 'Cantidad',
+			'price' => 'Precio',
+			'save' => 'Guardar',
+			'image' => 'No se han encontrado imágenes',
+			'Image' => 'Imagen',
+			'Stock' => 'Almacen',
+			'UnitPrice' => 'Precio unitario',
+			'sub-total' => "Sub total",
+			'total' => "Total",
+			'cost' => "Costo de envío",
+			'continue-shopping' => "Continuar compra",
+			'pay' => 'Pagar'
+		),
 	);
 
