900a600797404502bd47c4e7e9f26c9d93e016ae
ajpadilla <ajpadilla88@gmail.com>
2015-03-06 11:17:03 -0430

    Organizando secciones por productos y clasificados

--- app/views/products/search-result.blade.php
+++ app/views/products/search-result.blade.php
@@ -53,33 +53,66 @@
               </div>
               @include('products.partials.search._order-by-search')
             </div>
-      <!-- BEGIN PAGINATOR -->
+
+            @if(!$products->isEmpty())
+            <!-- BEGIN PAGINATOR -->
             <div id="total-items-1" class="row">
-              {{--<div class="col-md-4 col-sm-4 items-info">Productos {{ $products->getTotal()}}  en total</div>--}}
+              <div class="col-md-4 col-sm-4 items-info">Productos {{ $products->getTotal()}}  en total</div>
               <div class="col-md-8 col-sm-8">
-                 {{--{{ $products->links()}}>--}}
+                 {{ $products->links() }}
               </div>
             </div>
             <!-- END PAGINATOR -->
             <!-- BEGIN PRODUCT LIST -->
             <div id="products-list" class="row product-list">
               <!-- PRODUCT ITEM START -->
-              {{--@foreach($products as $product)
+              @foreach($products as $product)
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                     @include('products.partials._one-product')
                 </div>
-              @endforeach--}}
+              @endforeach
               <!-- PRODUCT ITEM END -->
             </div>
             <!-- END PRODUCT LIST -->
             <!-- BEGIN PAGINATOR -->
-            {{--<div id="total-items-2" class="row">
+            <div id="total-items-2" class="row">
               <div class="col-md-4 col-sm-4 items-info">Productos total</div>
               <div class="col-md-8 col-sm-8">
                 {{ $products->links()}}
               </div>
-            </div>--}}
+            </div>
             <!-- END PAGINATOR -->
+            @endif
+
+            @if(!$classifieds->isEmpty())
+            <!-- BEGIN PAGINATOR -->
+            <div id="total-items-classifieds-1" class="row margen">
+              <div class="col-md-4 col-sm-4 items-info">Clasificados {{ $classifieds->getTotal()}}  en total</div>
+              <div class="col-md-8 col-sm-8">
+                 {{ $classifieds->links() }}
+              </div>
+            </div>
+            <!-- END PAGINATOR -->
+            <!-- BEGIN PRODUCT LIST -->
+            <div id="classifieds-list" class="row classified-list">
+              <!-- PRODUCT ITEM START -->
+              @foreach($classifieds as $classified)
+                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                    @include('classifieds.partials._one-classified')
+                </div>
+              @endforeach
+              <!-- PRODUCT ITEM END -->
+            </div>
+            <!-- END PRODUCT LIST -->
+            <!-- BEGIN PAGINATOR -->
+            <div id="total-items-classifieds-2" class="row">
+              <div class="col-md-4 col-sm-4 items-info">Clasificados total</div>
+              <div class="col-md-8 col-sm-8">
+                {{ $classifieds->links()}}
+              </div>
+            </div>
+            <!-- END PAGINATOR -->
+            @endif
           </div>
           <!-- END CONTENT -->
         </div>
@@ -90,11 +123,18 @@
     </div>
 @stop
 
-{{--@if($products)
+@if($products)
   @foreach($products as $product)
     @include('products.partials._pop-up-products')
   @endforeach
-@endif--}}
+@endif
+
+@if($classifieds)
+  @foreach($classifieds as $classified)
+    @include('classifieds.partials._pop-up-classifieds')
+  @endforeach
+@endif
+
 
 @include('products.partials._product-list-tpl')
 
@@ -107,3 +147,13 @@
     });
   </script>
 @stop
+
+@section('styles')
+  <style type="text/css">
+    .margen{
+      margin-top: 40px;
+    }
+  </style>
+@stop
+
+
