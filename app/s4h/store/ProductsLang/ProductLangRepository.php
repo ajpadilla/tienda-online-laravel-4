d5fcf2be3177ef8c403649513b8f1ca274ef1d2d
ajpadilla <ajpadilla88@gmail.com>
2015-03-06 09:50:19 -0430

    Actualizando repositorio y clase

--- app/s4h/store/ProductsLang/ProductLangRepository.php
+++ app/s4h/store/ProductsLang/ProductLangRepository.php
@@ -20,7 +20,21 @@ use s4h\store\Base\BaseRepository;
       return new ProductLang;
     }
 
-    public $filters = ['filterWord'];
+    public $filters = ['filterWord','price','firstValue','secondValue'];
+  
+    public function filterByfilterWord($query, $data = array())
+    {
+      $language = $this->getCurrentLang();
+      $query->where('language_id', '=', $language->id)->where('name', 'LIKE', '%' . $data['filterWord'] . '%')->orWhere('description', 'LIKE', '%' . $data['filterWord'] . '%')->where('language_id', '=', $language->id);
+    }
+
+    public function filterByPrice($query, $data = array())
+    {
+      $language = $this->getCurrentLang();
+      $query->with('product')->whereHas('product', function($q) use ($data){
+          $q->whereBetween('price',[$data['firstValue'], $data['secondValue']]);
+      })->where('language_id', '=', $language->id);
+    }
 
     public function getProductForLanguage($productId, $languageId)
     {
