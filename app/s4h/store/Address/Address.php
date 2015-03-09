af8becacd0f8b854a4c113c9f921299884525665
ajpadilla <ajpadilla88@gmail.com>
2015-03-09 15:17:43 -0430

    Creando modelo Address

---
+++ app/s4h/store/Address/Address.php
@@ -0,0 +1,11 @@
+<?php namespace s4h\store\Address;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+/**
+ * 
+ */
+ class Address extends Eloquent{
+
+ 	protected $table = 'address';
+ } 
