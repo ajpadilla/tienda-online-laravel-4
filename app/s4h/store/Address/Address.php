b12b67278a8af4875acdf5554baf201249c11701
ajpadilla <ajpadilla88@gmail.com>
2015-03-10 10:57:43 -0430

    Agregando relacio, para los modelos city y people

--- app/s4h/store/Address/Address.php
+++ app/s4h/store/Address/Address.php
@@ -9,8 +9,16 @@ use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
  	protected $table = 'address';
 
- 	public function Classified(){
+ 	public function classified(){
 		return $this->hasOne('s4h\store\Classifieds\Classified','address_id');
+	}
+
+	public function city(){
+		return $this->belongsTo('s4h\store\Cities\City','city_id');
+	}
+
+	public function people(){
+		return $this->hasOne('s4h\store\Peoples\People','address_id');
 	}
 
  } 
