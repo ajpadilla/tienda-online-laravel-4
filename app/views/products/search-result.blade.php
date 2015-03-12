f0e5935072d9373c8cf8abb9cdd6409caaa89db1
ajpadilla <ajpadilla88@gmail.com>
2015-03-12 01:40:52 -0430

    Agregando sección, para cargar datos

--- app/views/products/search-result.blade.php
+++ app/views/products/search-result.blade.php
@@ -54,7 +54,8 @@
               @include('products.partials.search._order-by-search')
             </div>
 
-            @if(!$products->isEmpty())
+            <div id="result-section-search">
+               @if(!$products->isEmpty())
             <!-- BEGIN PAGINATOR -->
             <div id="total-items-produts-1" class="row">
               <div class="col-md-4 col-sm-4 items-info">Productos {{ $products->getTotal()}}  en total</div>
@@ -83,36 +84,39 @@
             </div>
             <!-- END PAGINATOR -->
             @endif
-
-            @if(!$classifieds->isEmpty())
-            <!-- BEGIN PAGINATOR -->
-            <div id="total-items-classifieds-1" class="row margen">
+        
+             @if(!$classifieds->isEmpty())
+             <!-- BEGIN PAGINATOR -->
+             <div id="total-items-classifieds-1" class="row margen">
               <div class="col-md-4 col-sm-4 items-info">Clasificados {{ $classifieds->getTotal()}}  en total</div>
               <div class="col-md-8 col-sm-8">
-                 {{ $classifieds->links() }}
-              </div>
+               {{ $classifieds->links() }}
+             </div>
+           </div>
+           <!-- END PAGINATOR -->
+           <!-- BEGIN PRODUCT LIST -->
+           <div id="classifieds-list" class="row classified-list">
+            <!-- PRODUCT ITEM START -->
+            @foreach($classifieds as $classified)
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+              @include('classifieds.partials._one-classified')
             </div>
-            <!-- END PAGINATOR -->
-            <!-- BEGIN PRODUCT LIST -->
-            <div id="classifieds-list" class="row classified-list">
-              <!-- PRODUCT ITEM START -->
-              @foreach($classifieds as $classified)
-                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                    @include('classifieds.partials._one-classified')
-                </div>
-              @endforeach
-              <!-- PRODUCT ITEM END -->
+            @endforeach
+            <!-- PRODUCT ITEM END -->
+          </div>
+          <!-- END PRODUCT LIST -->
+          <!-- BEGIN PAGINATOR -->
+          <div id="total-items-classifieds-2" class="row">
+            <div class="col-md-4 col-sm-4 items-info">Clasificados total</div>
+            <div class="col-md-8 col-sm-8">
+              {{ $classifieds->links()}}
             </div>
-            <!-- END PRODUCT LIST -->
-            <!-- BEGIN PAGINATOR -->
-            <div id="total-items-classifieds-2" class="row">
-              <div class="col-md-4 col-sm-4 items-info">Clasificados total</div>
-              <div class="col-md-8 col-sm-8">
-                {{ $classifieds->links()}}
-              </div>
-            </div>
-            <!-- END PAGINATOR -->
-            @endif
+          </div>
+          <!-- END PAGINATOR -->
+          @endif
+        </div>           
+
+           
           </div>
           <!-- END CONTENT -->
         </div>
