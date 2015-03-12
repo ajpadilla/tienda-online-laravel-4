62c7eaa0ed18290d8336b12df26f4d9a21553031
ajpadilla <ajpadilla88@gmail.com>
2015-03-12 01:39:48 -0430

    Actualizando rutas

--- app/routes.php
+++ app/routes.php
@@ -43,8 +43,6 @@ function () {
 		'uses' => 'ProductController@search'
 	]);
 
-	//Route::get('/ajax/search','ProductController@loadPage');
-
 	Route::get('returnDataProduct','ProductController@returnDataProduct');
 
 	Route::get('returnDataProductLang','ProductController@returnDataProductLang');
