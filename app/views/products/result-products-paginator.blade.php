eada5187f3a5d93d8e9a97c641ad160a1d029f4a
ajpadilla <ajpadilla88@gmail.com>
2015-03-12 01:44:18 -0430

    Agregando blade, para mostrar productos y clasificados paginados

---
+++ app/views/products/result-products-paginator.blade.php
@@ -0,0 +1,29 @@
+ @if(!$products->isEmpty())
+ <!-- BEGIN PAGINATOR -->
+ <div id="total-items-produts-1" class="row">
+  <div class="col-md-4 col-sm-4 items-info">Productos {{ $products->getTotal()}}  en total</div>
+  <div class="col-md-8 col-sm-8 links-products">
+   {{ $products->links() }}
+ </div>
+</div>
+<!-- END PAGINATOR -->
+<!-- BEGIN PRODUCT LIST -->
+<div id="products-list" class="row product-list">
+  <!-- PRODUCT ITEM START -->
+  @foreach($products as $product)
+  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+    @include('products.partials._one-product')
+  </div>
+  @endforeach
+  <!-- PRODUCT ITEM END -->
+</div>
+<!-- END PRODUCT LIST -->
+<!-- BEGIN PAGINATOR -->
+<div id="total-items-produts-2" class="row">
+  <div class="col-md-4 col-sm-4 items-info">Productos total</div>
+  <div class="col-md-8 col-sm-8 links-products">
+    {{ $products->links()}}
+  </div>
+</div>
+<!-- END PAGINATOR -->
+@endif
