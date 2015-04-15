79cda12558ecc69820fb7ddca902427e2563b46d
ajpadilla <ajpadilla88@gmail.com>
2015-04-15 14:42:07 -0430

    Agregando metodo listAll

---
+++ app/s4h/store/CardBrands/CardBrandsRepository.php
@@ -0,0 +1,40 @@
+<?php namespace s4h\store\CardBrands;
+
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use s4h\store\Languages\Language;
+use s4h\store\Base\BaseRepository;
+use s4h\store\CardBrands\CardBrands;
+
+class CardBrandsRepository extends BaseRepository
+{
+	public function getModel()
+    {
+      return new CardBrands;
+    }
+
+    public function getAll()
+    {
+        return CardBrands::all();
+    }
+
+    public function listAll()
+    {
+        return CardBrands::all()->lists('name','id');
+    }
+
+    public function create($data = array())
+    {
+    	//
+    }
+
+    public function update($data = array())
+    {
+    	//
+    }
+
+    public function delete($id)
+    {
+    	//
+    }
+
+}
