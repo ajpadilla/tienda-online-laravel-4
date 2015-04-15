05b7b4bf350f10070ed93676c80fa2a8c45e90f2
ajpadilla <ajpadilla88@gmail.com>
2015-04-15 14:17:55 -0430

    Actualizando traducciones

--- app/lang/en/PaymentCredentialDetails.php
+++ app/lang/en/PaymentCredentialDetails.php
@@ -1,6 +1,11 @@
 <?php 
-	
+
 return array(
+
+	"index-title" => "List Details payment credentials",
+	"create-title" => "Create Details payment credentials",
+	"sending" => 'Sending data',
+	
 	"routes" => array(
 		"show" => "PaymentCredentialDetails/show/{id}",
 		"create" => "PaymentCredentialDetails/create",
@@ -14,4 +19,28 @@ return array(
 			"saveLang" => 'PaymentCredentialDetails/api/update-lang',
 			),
 		),
-)
+
+	"labels" => array(
+		'new' => 'Nuevo',
+		'email' => 'Email',
+		'credit_cart_number' => 'Credit cart number',
+		'credit_cart_security_number' => 'Credit cart security number',
+		'credit_cart_expire_date'	=> 'Credit cart expire date',
+		'payments_types'		=> 'Payment  type',
+		'card_brands'	=> 'Card Brands',
+		'save' => 'Save'
+	),
+
+	'list' => array(
+		'email'	=> 'Email',
+		'credit_cart_number' => 'Credit cart number',
+		'credit_cart_security_number'		=> 'Credit cart security number',
+		'credit_cart_expire_date'	=> 'Credit cart expire date',
+		'payments_types'		=> 'Payment  type',
+		'users'		=> 'User',
+		'actions' => 'Actions',
+		'card_brands'	=> 'Card Brands',
+		'title' => 'List Details payment credentials',
+		'subtitle' => 'List details of payment credentials',
+		),
+	);
--- app/lang/es/PaymentCredentialDetails.php
+++ app/lang/es/PaymentCredentialDetails.php
@@ -1,6 +1,13 @@
 <?php 
-	
+
 return array(
+
+	'title' => 'Crear detalles de credenciales de pago',
+	'subtitle' => 'Agregar detalles de credenciales de pago',
+	"index-title" => "Lista de detalles de credenciales de pago",
+	"create-title" => "Crear detalles de credenciales de pago",
+	"sending" => 'Enviando datos',
+
 	"routes" => array(
 		"show" => "DetallesdecredencialesDePago/ver/{id}",
 		"create" => "DetallesdecredencialesDePago/crear",
@@ -14,5 +21,29 @@ return array(
 			"saveLang" => 'DetallesdecredencialesDePago/api/actualizar-idioma',
 			),
 		),
-)
+	"labels" => array(
+		'new' => 'Nuevo',
+		'email' => 'Email',
+		'credit_cart_number' => 'Nro de tarjeta de credito',
+		'credit_cart_security_numbe' => 'Nro de seguridad de tarjeta de credito',
+		'credit_cart_expire_date'	=> 'Fecha de vencimiento tarjeta de credito',
+		'payments_types'		=> 'Tipo de pago ',
+		'card_brands'	=> 'Marca de la tarjeta',
+		'save' => 'Guardar'
+	),
+
+	'list' => array(
+		'email'	=> 'Email',
+		'credit_cart_number' => 'Nro de tarjeta de credito',
+		'credit_cart_security_numbe'		=> 'Nro de seguridad tarjeta de credito',
+		'credit_cart_expire_date'	=> 'Fecha de vencimiento tarjeta de credito',
+		'payments_types'		=> 'Tipo de pago',
+		'users'		=> 'Usuario',
+		'actions' => 'Acciones',
+		'card_brands'	=> 'Marca de tarjeta de crediot',
+		'title' => 'Listado de detalles de credenciales de pago',
+		'subtitle' => 'Lista de detalles de credenciales de pago',
+		),
+
+	);
 
