ec47362c50641e4e207aad692cc637497a8256cd
ajpadilla <ajpadilla88@gmail.com>
2015-04-15 23:09:24 -0430

    Validando campo credit_cart_expire_date

--- app/s4h/store/Forms/EditCredentialForm.php
+++ app/s4h/store/Forms/EditCredentialForm.php
@@ -3,7 +3,9 @@
 use Laracasts\Validation\FormValidator;
 
 class EditCredentialForm extends FormValidator{
-	protected $rules = ['payments_types_id' => 'required|integer',
+	protected $rules = [
+		'credit_cart_expire_date' => 'date'
+		'payments_types_id' => 'required|integer',
 		'card_brands_id' => 'required|integer',
 		'users_id' => 'required|integer'
 	];
