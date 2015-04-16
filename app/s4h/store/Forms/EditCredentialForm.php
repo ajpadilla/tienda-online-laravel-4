e895235c1cfcc0134f80b33a60425be41d210185
ajpadilla <ajpadilla88@gmail.com>
2015-04-15 23:13:34 -0430

    Solucionando error

--- app/s4h/store/Forms/EditCredentialForm.php
+++ app/s4h/store/Forms/EditCredentialForm.php
@@ -4,7 +4,7 @@ use Laracasts\Validation\FormValidator;
 
 class EditCredentialForm extends FormValidator{
 	protected $rules = [
-		'credit_cart_expire_date' => 'date'
+		'credit_cart_expire_date' => 'date',
 		'payments_types_id' => 'required|integer',
 		'card_brands_id' => 'required|integer',
 		'users_id' => 'required|integer'
