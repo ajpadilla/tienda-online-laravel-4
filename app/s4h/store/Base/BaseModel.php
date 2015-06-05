e92eae0aa201815b868b5ff538bdfa0e558bffca
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-05 14:50:33 -0430

    Actualiando modelo

--- app/s4h/store/Base/BaseModel.php
+++ app/s4h/store/Base/BaseModel.php
@@ -2,20 +2,18 @@
 
 use Eloquent;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-use Datatable;
-use Illuminate\Database\Eloquent\Collection;
 use s4h\store\Languages\Language;
-
 
 /**
 * 
 */
 class BaseModel extends Eloquent{
-	
+
 	public function getCurrentLang(){
 		$isoCode = LaravelLocalization::setLocale();
 		$language = Language::select()->where('iso_code','=',$isoCode)->first();
 		return $language;
 	}
+
 }
 
