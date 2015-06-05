04902bfd745c21031a0086406d5cc2ae3554f60c
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-05 14:50:55 -0430

    Refactorizando metodos para clases repositories

--- app/s4h/store/Base/BaseRepository.php
+++ app/s4h/store/Base/BaseRepository.php
@@ -1,20 +1,87 @@
 <?php namespace s4h\store\Base;
 
-
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Datatable;
+use Illuminate\Database\Eloquent\Collection;
 use s4h\store\Languages\Language;
 
 /**
 * 
 */
-abstract class BaseRepository 
+class BaseRepository 
 {
 
+	protected $model;
+	protected $columns;	
+	protected $actionColums = array();
+	protected $collection;
+	protected $listAllRoute;
 	const PAGINATE = true;
-
 	public $filters = [];
 
-	abstract public function getModel();
+	public function getColumnCount()
+	{
+		return count($this->columns);
+	}
+
+	public function setListAllRoute($listAllRoute)
+	{
+		$this->listAllRoute = $listAllRoute;
+	}
+
+	public function getListAllRoute()
+	{
+		return $this->listAllRoute;
+	}
+
+	public function setModel($model)
+	{
+		$this->model = $model;
+	}
+
+	public function getModel()
+	{
+		return $this->model;
+	}
+
+	public function create($data = array())
+	{
+		$model = $this->model->create($data); 
+		return $model;
+	}
+
+	public function  getAll($exclude = null)
+	{
+		if($exclude)
+			return $this->getModel()->whereNotIn('id', $exclude)->get();
+		return $this->getModel()->all();
+	}	
+
+	public function getAllForSelect()
+	{
+		return $this->getAll()->lists('name', 'id');
+	}	
+	
+	public function delete($id)
+	{
+		$model = $this->get($id); 
+		return $model->delete();
+	}
+
+	public function get($id)
+	{
+		return $this->model->findOrFail($id);
+	}	
+
+	public function update($data = array()){}
+
+	public function getAllForCurrentLang(){}
+
+	public function getCurrentLang(){
+		$isoCode = LaravelLocalization::setLocale();
+		$language = Language::select()->where('iso_code','=',$isoCode)->first();
+		return $language;
+	}
 
 	public function search(array $data = array(), $paginate = false, $numItems = 10)
 	{
@@ -39,12 +106,6 @@ abstract class BaseRepository
 		$this->sortResults($query, $data);
 
 		return $paginate ? $query->paginate($numItems): $query->get();
-	}
-
-	public function getCurrentLang(){
-		$isoCode = LaravelLocalization::setLocale();
-		$language = Language::select()->where('iso_code','=',$isoCode)->first();
-		return $language;
 	}
 
 	public function sortResults($query, array $data = array())
