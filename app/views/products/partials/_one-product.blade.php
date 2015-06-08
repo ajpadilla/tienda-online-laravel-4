01f9b56c4c62aa80179f7c47f197488f5d1c633b
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-08 17:50:15 -0430

    Actualizando blade

--- app/views/products/partials/_one-product.blade.php
+++ app/views/products/partials/_one-product.blade.php
@@ -1,3 +1,4 @@
+ @if(!empty($product->inCurrentLang))
 <div class="product-item">
   <div class="pi-img-wrapper">
     @if($product->hasPhotos())
@@ -30,6 +31,6 @@
     @else
       <a href="{{ route('wishlist.create', $product->id) }}" class="add_wishlist btn btn-default add2cart"><i class="fa fa-check-square-o"></i></a>
     @endif    
-    <div class="sticker {{ $currentSticker }}"></div>
+    <div class="sticker {{ $currentSticker }}"></div>  
 </div>
-
+@endif
