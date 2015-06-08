ae293f44db4ecebfae3b1e98c7292d8f1381d220
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-08 17:46:23 -0430

    Refactorizando metodos para crear datatable

--- app/s4h/store/Base/BaseRepository.php
+++ app/s4h/store/Base/BaseRepository.php
@@ -83,6 +83,83 @@ class BaseRepository
 		return $language;
 	}
 
+	/*
+	************************** DATATABLE COLLECTION METHODS *********************************
+	*/
+
+	public function getAllTable($route = null, $params = array(), $orderColumn = 1, $type = 'asc', $tableId = 'datatable')
+	{
+		if(!$route)
+			$route = $this->listAllRoute;
+		
+		$datatable = Datatable::table();
+		$datatable->addColumn($this->columns);
+		$datatable->setOptions('order', [[$orderColumn , $type]]);
+		//$datatable->setCustomValues('table-id', 'datatable-' . $tableId);
+		if($tableId != 'datatable')
+			$datatable->setId('datatable-' . $tableId);
+		$datatable->setUrl(route($route, $params));
+		$datatable->noScript();	
+		return $datatable;
+	}	
+
+	public function setCollection($collection)
+	{
+		$this->collection = Datatable::collection($collection);
+	}	
+
+	public function setDatatableCollection(Collection $collection)
+	{
+		$this->collection = Datatable::collection($collection);
+	}
+
+	public function addColumnToCollection($name, $content)
+	{
+		$this->collection->addColumn($name, $content);
+	}
+
+	public function addActionColumn($column)
+	{
+		$this->actionColums[] = $column;
+	}
+
+	public function getActionColumn()
+	{
+		return $this->actionColums;
+	}
+
+	public function cleanActionColumn()
+	{
+		unset($this->actionColums);
+		$this->actionColums = array();
+	}
+
+	public function getTableCollectionForRender() {
+		return $this->collection->make();
+	}
+
+	public function getDefaultTableForAll()
+	{
+		$collection = $this->getAll();
+		$this->setDatatableCollection($collection);
+		$this->setDefaultTableSettings();
+		return $this->getTableCollectionForRender();
+	}		
+
+	public function setDefaultTableSettings()
+	{
+		$this->setBodyTableSettings();
+		$this->setDefaultActionColumn();
+	}	
+
+	public function setDefaultActionColumn(){}
+	public function setBodyTableSettings(){}
+	public function deleteImageDirectory($id){}
+
+	/*
+	************************** SEARCH METHODS *********************************
+	*/
+
 	public function search(array $data = array(), $paginate = false, $numItems = 10)
 	{
 		$language = $this->getCurrentLang();
