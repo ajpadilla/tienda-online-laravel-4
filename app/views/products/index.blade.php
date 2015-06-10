4b0ec826670d5b870285ab91c977e763def8b7ec
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-10 00:02:50 -0430

    Agregando metodo fancyConfirm

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -99,9 +99,27 @@
 			$(".table").delegate(".delete-product", "click", function() {
              	action = getAttributeIdActionSelect($(this).attr('id'));
              	//console.log(action);
-             	deleteProduct(action.number);
+             	//deleteProduct(action.number);
+             	fancyConfirm('Are you sure you want to delete?', deleteProduct , action.number);
         	});
-			
+
+			function fancyConfirm(msg, deleteProduct, productId)
+			{
+				jQuery.fancybox({
+					'modal' : true,
+					'content' : "<div style=\"margin:1px;width:240px;\">"+msg+"<div style=\"text-align:right;margin-top:10px;\"><input id=\"fancyconfirm_cancel\" style=\"margin:3px;padding:0px;\" type=\"button\" value=\"Cancel\"><input id=\"fancyConfirm_ok\" style=\"margin:3px;padding:0px;\" type=\"button\" value=\"Ok\"></div></div>",
+					'beforeShow' : function() {
+						jQuery("#fancyconfirm_cancel").click(function() {
+							$.fancybox.close();
+						});
+
+						jQuery("#fancyConfirm_ok").click(function() {
+							$.fancybox.close();
+							deleteProduct(productId)
+						});
+					}
+				});
+			}
 
 			var loadDataToEditProduct = function(productId) 
 			{
