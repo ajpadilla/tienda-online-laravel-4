ddad5bc98262d73a31cb61c79b83ba9a32100873
Alvaro Padilla <ajpadilla88@gmail.com>
2015-06-10 22:36:14 -0430

    Actualizando metodos index, create

--- app/controllers/ClassifiedController.php
+++ app/controllers/ClassifiedController.php
@@ -55,126 +55,15 @@ class ClassifiedController extends \BaseController {
 	 */
 	public function index()
 	{
-		$languages = $this->languageRepository->getAll()->lists('name', 'id');
-		$classified_conditions = $this->classifiedConditionsRepository->getNameForLanguage(); 
-		$classified_types = $this->classifiedTypesRepository->getNameForLanguage();
-		$categories = $this->categoryRepository->getNameForLanguage();
-		return View::make('classifieds.index', compact('languages','classified_conditions','classified_types','categories'));
+		$languages = $this->languageRepository->getAllForSelect();
+		$classified_conditions = $this->classifiedConditionsRepository->getAllForCurrentLang(); 
+		$classified_types = $this->classifiedTypesRepository->getAllForCurrentLang();
+		$categories = $this->categoryRepository->getAllForCurrentLang();
+		$table = $this->repository->getAllTable();
+		return View::make('classifieds.index', compact('languages','classified_conditions','classified_types','categories','table'));
 	}
 
-	public function getDatatable()
-	{
-		$collection = Datatable::collection($this->classifiedLangRepository->getAllForLanguage($this->languageRepository->returnLanguage()->id))
-			->searchColumns('name','description', 'address')
-			->orderColumns('name','description', 'address');
-
-		$collection->addColumn('photo', function($model)
-		{
-			$links = '';
-			
-			$photo = $model->classified->getFirstPhoto();
-
-			if ($photo != false) {
-				$links .= "	<a href='#'>
-									<img class='mini-photo' alt='" . $photo->filename . "' src='" . asset($photo->complete_path) . "'>
-				</a>";
-			}
-				
-			return $links;
-		});
-
-		$collection->addColumn('name', function($model)
-		{
-			return $model->name;
-		});
-		
-		$collection->addColumn('description', function($model)
-		{
-			return $model->description;
-		});
-
-		$collection->addColumn('address', function($model)
-		{
-			return $model->address;
-		});
-
-		$collection->addColumn('price', function($model)
-		{
-			return $model->classified->price;
-		});
-
-		
-		$collection->addColumn('user', function($model)
-		{
-			return $model->classified->user->username;
-		});
-
-		$collection->addColumn('category', function($model)
-		{
-			$language = $this->languageRepository->returnLanguage();
-
-			if($model->classified->hasCategories())
-			{
-				$classifiedsCategoriesName = $model->classified->getCategories();
-				$links = '<select class="form-control m-b">';
-				foreach ($classifiedsCategoriesName as $category) {
-					$links .= '<option>'.$category.'</option>';
-				}
-				$links .='</select>';
-				return $links;
-			}
-			return '';
-		});
-
-		$collection->addColumn('classified_type', function($model)
-		{
-			$language = $this->languageRepository->returnLanguage();
-			$classified_type_language = $model->classified->classifiedType->languages()->where('language_id','=',$language->id)->first();
-			return $classified_type_language->pivot->name;
-		});
-
-		$collection->addColumn('classified_condition', function($model)
-		{
-			$language = $this->languageRepository->returnLanguage();
-			$classified_condition_language = $model->classified->classifiedCondition->languages()->where('language_id','=',$language->id)->first();
-			return $classified_condition_language->pivot->name;
-		});
-
-		$collection->addColumn('Actions',function($model){
-			
-			$languageId = $this->languageRepository->returnLanguage()->id;
-
-			$links = "<form action='".route('classifieds.show',$model->classified->id)."' method='get'>
-						<button href='#'  class='btn btn-success btn-outline dim col-sm-8 show' style='margin-left: 20px;' type='submit' data-toggle='tooltip' data-placement='top' title='".trans('classifieds.actions.Show')."'  data-original-title='".trans('classifieds.actions.Show')."' ><i class='fa fa-check fa-2x'></i></button><br/>
-					  </form>";
-
-			$links.= "<button href='#fancybox-edit-classified' id='edit_".$model->classified->id."' class='btn btn-warning btn-outline dim col-sm-8 edit' style='margin-left: 20px; ' type='button' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Edit')."'  data-original-title='".trans('products.actions.Edit')."' ><i class='fa fa-pencil fa-2x'></i>
-					 </button><br/>";
-
-			$links.= "<button href='#' class='btn btn-danger btn-outline dim col-sm-8 delete' id='delet_".$model->classified->id."' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Delete')."'  data-original-title='".trans('products.actions.Delete')."' ><i class='fa fa-times fa-2x'></i>
-					 </button><br/>";
-					 
-			if ($model->classified->active)
-			{
-				$links.= "<button href='#' class='btn btn-primary btn-outline dim col-sm-8 deactivated' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Activate')."'  data-original-title='".trans('products.actions.Deactivated')."'> <i class='fa fa-check fa-2x'></i></button><br />";
-			}
-			else
-			{
-				$links.= "<button href='#' class='btn btn-danger btn-outline dim col-sm-8 activate' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Deactivated')."'  data-original-title='".trans('products.actions.Activate')."'> <i class='fa fa-check fa-2x'></i></button><br />";
-			}
-
-			$links.= "<form action='".route('photoClassified.create',array($model->classified->id, $languageId))."' method='get'>
-							<button href='#' class='btn btn-info btn-outline dim col-sm-8 photo' style='margin-left: 20px' type='submit' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Photo')."'  data-original-title='".trans('products.actions.Photo')."'> <i class='fa fa-camera fa-2x'></i></button><br />
-					  </form>";
-
-			$links.= "<button href='#fancybox-edit-language-classified' id='language_".$model->classified->id."'  class='btn btn-success btn-outline dim col-sm-8 language' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('products.actions.Language')."'  data-original-title='".trans('products.actions.Language')."'> <i class='fa fa-pencil fa-2x'></i></button><br />";
-
-			return $links;
-		});
-
-		return $collection->make();
-	}
-
+	
 	/**
 	 * Show the form for creating a new resource.
 	 *
@@ -236,12 +125,11 @@ class ClassifiedController extends \BaseController {
 	 */
 	public function show($id)
 	{
-		$classified = $this->repository->getById($id);
-		$language = $this->languageRepository->returnLanguage();
-		$classifiedType = $classified->classifiedType->languages()->where('language_id','=',$language->id)->first();
-		$classifiedConditions = $classified->classifiedCondition->languages()->where('language_id','=',$language->id)->first();
-		$classified_language = $classified->languages()->where('language_id','=', $language->id)->first();
-		return View::make('classifieds.show', compact('classified','classifiedType','classifiedConditions','classified_language'));
+		$classified = $this->repository->get($id);
+		$classifiedType = $classified->classifiedType->InCurrentLang;
+		$classifiedConditions = $classified->classifiedCondition->InCurrentLang;
+		$classifiedLanguage = $classified->InCurrentLang;
+		return View::make('classifieds.show', compact('classified','classifiedType','classifiedConditions','classifiedLanguage'));
 	}
 
 
@@ -471,6 +359,11 @@ class ClassifiedController extends \BaseController {
 			return Response::json(['success' => true]);
 		}
 		return Response::json(['success' => false]);
+	}
+
+	public function listApi()
+	{
+		return $this->repository->getDefaultTableForAll();
 	}
 
 }
