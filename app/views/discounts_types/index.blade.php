542babf82bcc7029fb62bf18b8ea7e97a69564a0
ajpadilla <ajpadilla88@gmail.com>
2015-07-15 14:46:55 -0430

    Agregando formulario, para crear/editar tipos de descuentos

--- app/views/discounts_types/index.blade.php
+++ app/views/discounts_types/index.blade.php
@@ -21,7 +21,146 @@
 		</div>
 	</div>
 </div>
+
+<div class="row" style="display: none">
+	<section id="fancybox-edit-discount-type">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>{{	trans('products.subtitle') }}</h5>
+					</div>
+					<div class="ibox-content">
+						<div class="row">
+							{{Form::open(['route' => 'discountType.api.update', 'class' => 'form-horizontal', 'id' => 'form-edit-discount-type'])}}
+								@include('discounts_types.partials._form')
+							{{Form::close()}}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
 @stop
 
 @section('scripts')
+<script type="text/javascript">
+	$(document).ready(function () 
+	{
+		$(".table").delegate(".edit-discount-type", "click", function() {
+			action = getAttributeIdActionSelect($(this).attr('id'));
+	       // console.log(action);
+	        loadDataToEditDiscountType(action.number);
+	    });
+
+		var loadDataToEditDiscountType = function(discountTypeId) 
+		{
+			$.ajax({
+				type: 'GET',
+				url: '{{ URL::route('discountType.api.show') }}',	
+				data: {'discountTypeId': discountTypeId},
+				dataType: "JSON",
+				success: function(response) 
+				{
+					//console.log(response);
+					if (response.success == true) 
+					{
+						$('#language_id').val(response.discountType.discountTypeLang.language_id);
+						$('#discount_type_id').val(response.discountType.attributes.id);
+						$('#name_discount_type').val(response.discountType.discountTypeLang.name);
+						showPopUpFancybox('#fancybox-edit-discount-type');
+					}
+				}
+			});
+		};
+
+		$('#form-edit-discount-type').validate({
+			rules:{
+				name:{
+					required:!0,
+					onlyLettersNumbersAndSpaces: true,
+					/*remote:
+						{
+							url:'{{ URL::to('/checkName/') }}',
+							type: 'POST',
+							data: {
+								name: function() {
+									return $('#name').val();
+								},
+								language_id: function () {
+									return $('#language_id').val();
+								}
+
+							},
+							dataFilter: function (respuesta) {
+								console.log('consulta:'+respuesta);
+								return respuesta;
+							}
+						}*/
+				},
+			},
+			messages:{
+				name:{
+					required: '{{ trans('discountType.validation.required') }}',
+					rangelength: '{{ trans('discountType.validation.rangelength') }}'+'[2, 45]'+'{{ trans('discounts.validation.characters') }}',
+					remote: jQuery.validator.format('{{ trans('discountType.alert') }}')
+				},
+			},
+			highlight:function(element){
+				$(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+			},
+			unhighlight:function(element){
+				$(element).closest('.form-group').removeClass('has-error');
+			},
+			success:function(element){
+				element.addClass('valid').closest('.form-group').removeClass('has-error').addClass('has-success');
+			}  
+		});
+
+		var options = { 
+				beforeSubmit:  showRequest,  // pre-submit callback 
+				success:       showResponse,  // post-submit callback 
+				url: '{{URL::route('discountType.api.update')}}',
+        		type:'POST'
+			};
+
+		$('#form-edit-discount-type').ajaxForm(options);
+	});
+	
+	// pre-submit callback 
+		function showRequest(formData, jqForm, options) {          
+			setTimeout(jQuery.fancybox({
+				'content':'<h1>' + '{{ trans('discountType.sending') }}' + '</h1>',
+				'autoScale' : true,
+				'transitionIn' : 'none',
+				'transitionOut' : 'none',
+				'scrolling' : 'no',         
+				'type' : 'inline',
+				'showCloseButton' : false,
+				'hideOnOverlayClick' : false,
+				'hideOnContentClick' : false    
+			}), 5000 );  
+			return $('#form-edit-discount-type').valid(); 
+		} 
+														     
+		// post-submit callback 
+		function showResponse(responseText, statusText, xhr, $form)  {    
+			if(responseText.success) 
+			{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.message + '</h1>',
+					'autoScale' : true
+				});
+				reloadDataTable('#datatable');
+			}else{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.errors + '</h1>',
+					'autoScale' : true
+				});
+			}
+		} 
+
+</script>
 @stop
--- app/views/discounts_types/partials/_form.blade.php
+++ app/views/discounts_types/partials/_form.blade.php
@@ -5,11 +5,18 @@
 			{{ Form::select('language_id',$languages,null,array('class' => 'form-control','id'=>'language_id')) }}
 		</div>
 	</div>
+	
+	<div class="form-group" style="display: none">
+		{{ Form::label('discount_type_id','id', ['class' => 'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			{{ Form::text('discount_type_id', null, ['class' => 'form-control', 'placeholder' =>'','id'=> 'discount_type_id']) }}
+		</div>
+	</div>
 
 	<div class="form-group">
 		{{ Form::label('name',trans('discountType.labels.name'),['class'=>'col-sm-2 control-label']) }}
 		<div class="col-sm-8">
-			{{ Form::text('name',null, ['class' => 'form-control','id'=>'name']) }}
+			{{ Form::text('name',null, ['class' => 'form-control','id'=>'name_discount_type']) }}
 		</div>
 	</div>
 </div>
