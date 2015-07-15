32af03593fb8603dcafa23bce4f5b8dbb4b51c09
ajpadilla <ajpadilla88@gmail.com>
2015-07-15 15:39:22 -0430

    Actualizando formulario

--- app/views/categories/partials/_form_language.blade.php
+++ app/views/categories/partials/_form_language.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-7">
 
 	<div class="form-group">
-		{{ Form::label('language_id', trans('categories.labels.language'),['class'=>'col-sm-2 control-label col-lg-offset-2']) }}
+		{{ Form::label('language_id', trans('categories.labels.language'),['class'=>'col-sm-2 control-label']) }}
 		<div class="col-sm-10">
 			{{ Form::select('language_id',$languages,null,['class' => 'form-control','id'=>'lang_id']) }}
 		</div>
@@ -22,7 +22,7 @@
 	</div>
 </div>
 
-<div class="col-lg-6 col-lg-offset-2">
+<div class="col-lg-6">
 	<div class="form-group">
 		<div class="col-sm-3">
 			<button type="submit" class="pull-right btn btn-primary" name="add_photos" value="0">Guardar</button>
