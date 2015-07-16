0c45ae2d9737eea33c26384641e3425bce1d7503
ajpadilla <ajpadilla88@gmail.com>
2015-07-16 16:16:22 -0430

    Refactorizando blade

--- app/views/invoice_status/index.blade.php
+++ app/views/invoice_status/index.blade.php
@@ -15,20 +15,8 @@
 			@include('flash::message')
 			<div class="ibox-content">
 			<a class="btn btn-info " href="{{ route('invoiceStatus.create') }}"><i class="fa fa-paste"></i> {{ trans('invoiceStatus.labels.new') }} </a>
-				<?php
-					$columns = [
-						trans('invoiceStatus.list.Color'),
-						trans('invoiceStatus.list.Name'),
-						trans('invoiceStatus.list.Description'),
-						trans('invoiceStatus.list.Actions')
-				];
-				$table = Datatable::table()
-				->addColumn($columns)
-				->setUrl(route('api.invoiceStatus'))
-				->noScript();
-				?>
 				<div class="row"><br/></div>
-				{{ $table->render() }}
+				@include('partials._index-table')
 			</div>
 		</div>
 	</div>
@@ -44,7 +32,7 @@
 					</div>
 					<div class="ibox-content">
 						<div class="row">
-							{{Form::open(['route' => 'invoiceStatus.update', 'class' => 'form-horizontal', 'id' => 'formEditInvoiceStatus'])}}
+							{{Form::open(['route' => 'invoiceStatus.api.update', 'class' => 'form-horizontal', 'id' => 'form-edit-invoice-status'])}}
 								@include('invoice_status.partials._form')
 							{{Form::close()}}
 						</div>
@@ -65,7 +53,7 @@
 					</div>
 					<div class="ibox-content">
 						<div class="row">
-							{{Form::open(['route' => 'invoiceStatus.saveLang', 'class' => 'form-horizontal', 'id' => 'formEditInvoiceStatusLanguage'])}}
+							{{Form::open(['route' => 'invoiceStatus.api.saveLang', 'class' => 'form-horizontal', 'id' => 'form-edit-invoice-status-language'])}}
 								@include('invoice_status.partials._form_language')
 							{{Form::close()}}
 						</div>
@@ -85,115 +73,82 @@
 	<script type="text/javascript">
 		$(document).ready(function () 
 		{
-			$('.summernote').summernote();
-
 			$('select[name="color"]').simplecolorpicker({theme: 'glyphicons'});
+				
+			$(".table").delegate(".edit-invoice-status", "click", function() {
+				action = getAttributeIdActionSelect($(this).attr('id'));
+	      		//console.log(action);
+	        	loadDataToEditInvoiceStatus(action.number);
+	    	});
 
-			loadData();
+	    	$(".table").delegate(".edit-invoice-status-lang", "click", function() {
+             	action = getAttributeIdActionSelect($(this).attr('id'));
+             	//console.log(action);
+             	loadDataForInvocieStatusLang(action.number);
+        	});
 
-			$('.edit').fancybox({
-				openEffect	: 'elastic',
-	    		closeEffect	: 'elastic',
-				centerOnScroll: true,
-				hideOnOverlayClick: true,
-				beforeLoad: loadData()
-			});
+			$(".table").delegate(".delete-invoice-status", "click", function() {
+        		action = getAttributeIdActionSelect($(this).attr('id'));
+        		//console.log(action);
+        		fancyConfirm('Are you sure you want to delete?', deleteInvoiceStatus , action.number);
+        	});
 
-
-			$('.language').fancybox({
-				openEffect	: 'elastic',
-	    		closeEffect	: 'elastic',
-				centerOnScroll: true,
-				hideOnOverlayClick: true,
-				beforeLoad: loadData()
-			});
-
-			function loadData() 
+			var loadDataToEditInvoiceStatus = function(invoiceStatusId) 
 			{
-				$('.table').click(function(event)
-				{
-					var target = $( event.target );
-					if (target.is('button')) 
+				$.ajax({
+					type: 'GET',
+					url: '{{ URL::route('invoiceStatus.api.show') }}',	
+					data: {'invoiceStatusId': invoiceStatusId},
+					dataType: "JSON",
+					success: function(response) 
 					{
-						console.log($(target).attr('id'));
-
-						var id = $(target).attr('id');
-						var type = id ? id.split('_')[0] : '';
-						var numberId = id ? id.split('_')[1] : '';
-
-						if (type == "edit") 
+						//console.log(response);
+						if (response.success == true) 
 						{
-							loadDataToEdit(numberId);
+							$('#language_id').val(response.invoiceStatus.invoiceStatusLang.language_id);
+							$('#invoice_status_id').val(response.invoiceStatus.attributes.id);
+							$('#name_invoice_status').val(response.invoiceStatus.invoiceStatusLang.name);
+							if (response.invoiceStatus.attributes.color != '') {
+								$('select[name="color"]').simplecolorpicker('selectColor', response.invoiceStatus.attributes.color);
+							};
+							$('#description_invoice_status').code(response.invoiceStatus.invoiceStatusLang.description);
+							showPopUpFancybox('#fancybox-edit-invoice-status');
 						}
-						else
-						{
-							if (type == "language" ) 
-							{
-								loadDataForLanguage(numberId);
-							}
-							else
-							{
-								if (type == "delet")
-								{
-									deleteinvoiceStatus(numberId);
-								}
-							}
-						}
-
-					}			
+					}
 				});
-			}
+			};
 
-			function loadDataToEdit(id) 
+
+			var loadDataForInvocieStatusLang = function (invoiceStatusId) 
 			{
 				$.ajax({
 					type: 'GET',
-					url: '{{ URL::to('/returnDataInvoiceStatus/') }}',	
-					data: {'invoiceStatusId': id},
+					url: '{{ URL::route('invoiceStatus.api.show') }}',	
+					data: {'invoiceStatusId': invoiceStatusId},
 					dataType: "JSON",
 					success: function(response) {
-						console.log(response.invoice_status.invoice_status_lang);
-						if (response.success == true) {
-							$('#invoice_status_id').val(response.invoice_status_lang.invoice_status_id);
-							$('#language_id').val(response.invoice_status_lang.language_id);
-							$('#name').val(response.invoice_status_lang.name);
-							$('#description').code(response.invoice_status_lang.description);
-							if (response.invoice_status.color != '') {
-								$('select[name="color"]').simplecolorpicker('selectColor', response.invoice_status.color);
-							};
+						//console.log(response.product);
+						if (response.success == true) 
+						{
+							$('#invoice_status_id_lang').val(response.invoiceStatus.attributes.id);
+							$('#lang_id').val(response.invoiceStatus.invoiceStatusLang.language_id);
+							$('#name_language').val(response.invoiceStatus.invoiceStatusLang.name);
+							$('#description_language').code(response.invoiceStatus.invoiceStatusLang.description);
+							showPopUpFancybox('#form-edit-invoice-status-language');
 						}
 					}
 				});
-			}
-
-			function loadDataForLanguage(id) {
-				$.ajax({
-					type: 'GET',
-					url: '{{ URL::to('/returnDataInvoiceStatus/') }}',	
-					data: { 'invoiceStatusId': id },
-					dataType: "JSON",
-					success: function(response) {
-						console.log(response.invoice_status_lang);
-						if (response.success == true) {
-							$('#invoice_status_id_lang').val(response.invoice_status_lang.invoice_status_id);
-							$('#lang_id').val(response.invoice_status_lang.language_id);
-							$('#name_language').val(response.invoice_status_lang.name);
-							$('#description_language').code(response.invoice_status_lang.description);
-						}
-					}
-				});
-			}
+			};
 
 			$('#lang_id').click(function () {
-				console.log($('#invoice_status_id_lang').val() +" "+ $('#lang_id').val());
-
+				//console.log($('#shipment_status_id_language').val() +" "+ $('#lang_id').val());
 				$.ajax({
 					type: 'GET',
-					url: '{{ URL::to('/returnDatainvoiceStatusLang/') }}',	
+					url: '{{ URL::route('invoiceStatus.api.show-lang') }}',	
 					data: {'invoiceStatusId':$('#invoice_status_id_lang').val(), 'languageId':$('#lang_id').val()},
 					dataType: "JSON",
 					success: function(response) {
-						console.log(response);
+						//console.log(response);
 						if (response.success == true) {
 							$('#name_language').val(response.invoiceStatusLang.name);
 							$('#description_language').code(response.invoiceStatusLang.description);
@@ -203,29 +158,24 @@
 						}
 					}
 				});
-
 			});
 
-			
-			function deleteinvoiceStatus(id) {
+			function deleteInvoiceStatus(invoiceStatusId) {
 				$.ajax({
 					type: 'GET',
-					url: '{{ URL::route('invoiceStatus.delete-ajax') }}',
-					data: {'invoiceStatusId': id},
+					url: '{{ URL::route('invoiceStatus.api.delete') }}',
+					data: {'invoiceStatusId': invoiceStatusId},
 					dataType: "JSON",
 					success: function(response) {
 						if (response.success == true) {
-							$('#delet_'+id).parent().parent().remove();
+							$('#delet_invoice-status_'+invoiceStatusId).parent().parent().remove();
+							reloadDataTable('#datatable');
 						};
 					}
 				});
 			}
-
-			$.validator.addMethod('onlyLettersNumbersAndSpaces', function(value, element) {
-					return this.optional(element) || /^[a-zA-Z0-9ñÑ\s]+$/i.test(value);
-			}, 'only letters, numbers and spaces.');
-
-			$('#formEditInvoiceStatus').validate({
+			
+			$('#form-edit-invoice-status').validate({
 				rules:{
 					name:{
 						required:true,
@@ -248,7 +198,7 @@
 				}
 			});
 
-			$('#formEditInvoiceStatusLanguage').validate({
+			$('#form-edit-invoice-status-language').validate({
 				rules:{
 					name:{
 						required:true,
@@ -274,19 +224,19 @@
 			var options = {
 						beforeSubmit:  showRequest,  // pre-submit callback
 						success:       showResponse,  // post-submit callback
-						url:  '{{ URL::route('invoiceStatus.update') }}',
+						url:  '{{ URL::route('invoiceStatus.api.update') }}',
 						type:'POST'
 					};
 
 			var optionsLang = {
 				beforeSubmit:  showRequestLang,  // pre-submit callback
 				success:       showResponseLang,  // post-submit callback
-				url:  '{{ URL::route('invoiceStatus.saveLang') }}',
+				url:  '{{ URL::route('invoiceStatus.api.saveLang') }}',
 				type:'POST'
 			}
 
-			$('#formEditInvoiceStatus').ajaxForm(options);
-			$('#formEditInvoiceStatusLanguage').ajaxForm(optionsLang);
+			$('#form-edit-invoice-status').ajaxForm(options);
+			$('#form-edit-invoice-status-language').ajaxForm(optionsLang);
 
 		});
 
@@ -303,15 +253,24 @@
 				'hideOnOverlayClick' : false,
 				'hideOnContentClick' : false
 			}), 5000 );
-			return $('#formEditInvoiceStatus').valid();
+			return $('#form-edit-invoice-status').valid();
 		}
 
 		// post-submit callback
 		function showResponse(responseText, statusText, xhr, $form)  {
-			jQuery.fancybox({
-				'content' : '<h1>'+ responseText + '</h1>',
-				'autoScale' : true
-			});
+			if(responseText.success) 
+			{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.message + '</h1>',
+					'autoScale' : true
+				});
+				reloadDataTable('#datatable');
+			}else{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.errors + '</h1>',
+					'autoScale' : true
+				});
+			}
 		}
 
 		// pre-submit callback
@@ -327,17 +286,24 @@
 				'hideOnOverlayClick' : false,
 				'hideOnContentClick' : false
 			}), 5000 );
-			return $('#formEditInvoiceStatusLanguage').valid();
+			return $('#form-edit-invoice-status-language').valid();
 		}
 
 		// post-submit callback
 		function showResponseLang(responseText, statusText, xhr, $form)  {
-			console.log(responseText);
-
-			jQuery.fancybox({
-				'content' : '<h1>'+ responseText + '</h1>',
-				'autoScale' : true
-			});
+			if(responseText.success) 
+			{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.message + '</h1>',
+					'autoScale' : true
+				});
+				reloadDataTable('#datatable');
+			}else{
+				jQuery.fancybox({
+					'content' : '<h1>'+ responseText.errors + '</h1>',
+					'autoScale' : true
+				});
+			}
 		}
 	</script>
 @stop
