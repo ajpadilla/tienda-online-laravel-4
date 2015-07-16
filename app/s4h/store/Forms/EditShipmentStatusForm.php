fc2b7697a3fb934f21611a4a92a2ca9e5c046960
ajpadilla <ajpadilla88@gmail.com>
2015-07-16 01:08:05 -0430

    Actualizando reglas de validación

--- app/s4h/store/Forms/EditShipmentStatusForm.php
+++ app/s4h/store/Forms/EditShipmentStatusForm.php
@@ -6,6 +6,7 @@ class EditShipmentStatusForm extends FormValidator{
 	protected $rules = [
                     'name' => 'required|max:255',
                     'description' => 'required',
-                    'color' => 'required|max:7'
+                    'color' => 'required|max:7',
+                    'shipment_status_id' => "required|exists:shipment_status,id"
      ];
 }
