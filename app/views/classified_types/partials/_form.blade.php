ff640c62a3ceeda74502948d877cddb6747632c1
ajpadilla <ajpadilla88@gmail.com>
2015-07-16 20:12:46 -0430

    Agregando blade, para formulario

---
+++ app/views/classified_types/partials/_form.blade.php
@@ -0,0 +1,22 @@
+<div class="col-sm-6 b-r">
+	<div class="form-group">
+		{{ Form::label('language_id', trans('classifiedTypes.labels.language'),['class'=>'col-sm-2 control-label']) }}
+		<div class="col-sm-6">
+			{{ Form::select('language_id',$languages,null,array('class' => 'form-control','id'=>'language_id')) }}
+		</div>
+	</div>
+	<div class="form-group">
+		{{ Form::label('name', trans('classifiedTypes.labels.name'),['class'=>'col-sm-2 control-label']) }}
+		<div class="col-sm-6">
+			{{ Form::text('name',null, ['class' => 'form-control', 'id' => 'name']) }}
+		</div>
+	</div>
+</div>
+
+<div class="col-sm-6">
+	<div class="form-group">
+		<div class="col-sm-4 col-sm-offset-2">
+			{{ Form::submit(trans('classifiedTypes.labels.save'), ['class' => 'btn btn-primary']) }}
+		</div>
+	</div>
+</div>
