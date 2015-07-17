9a94930fab1cc20a37b7a0daec19075702fa703a
ajpadilla <ajpadilla88@gmail.com>
2015-07-17 16:26:58 -0430

    Agregando atributo id a campos

--- app/views/classified_conditions/partials/_form.blade.php
+++ app/views/classified_conditions/partials/_form.blade.php
@@ -6,12 +6,21 @@
 			{{ Form::select('language_id',$languages,null,array('class' => 'form-control','id'=>'language_id')) }}
 		</div>
 	</div>
+	
+	<div class="form-group" style="display: none">
+		{{ Form::label('classified_conditions_id','id', ['class' => 'col-sm-2 control-label']) }}
+		<div class="col-sm-10">
+			{{ Form::text('classified_conditions_id', null, ['class' => 'form-control', 'placeholder' =>'','id'=> 'classified_conditions_id']) }}
+		</div>
+	</div>
+
 	<div class="form-group">
 		{{ Form::label('name', trans('classifiedConditions.labels.name'),['class'=>'col-sm-2 control-label']) }}
 		<div class="col-sm-6">
-			{{ Form::text('name',null, ['class' => 'form-control', 'id' => 'name']) }}
+			{{ Form::text('name',null, ['class' => 'form-control', 'id' => 'name_classified_conditions']) }}
 		</div>
 	</div>
+
 </div>
 
 <div class="col-sm-6">
