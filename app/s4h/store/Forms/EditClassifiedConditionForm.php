62e3debbcbf3179ab0d08c4ebef155c5e11ec2ec
ajpadilla <ajpadilla88@gmail.com>
2015-07-17 17:42:25 -0430

    Editando validaciones

--- app/s4h/store/Forms/EditClassifiedConditionForm.php
+++ app/s4h/store/Forms/EditClassifiedConditionForm.php
@@ -6,6 +6,8 @@ use Laracasts\Validation\FormValidator;
 */
 class EditClassifiedConditionForm extends FormValidator{
 	protected $rules = [
+		'language_id' => 'required|integer|exists:languages,id',
+		'classified_condition_id' => 'required|integer|exists:classified_conditions,id',
 		'name' => 'required|max:128',
 	];
 }
