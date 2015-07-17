96d229c8a90419263ada9414ad1acbbc82d5db6c
ajpadilla <ajpadilla88@gmail.com>
2015-07-17 16:43:18 -0430

    Agregando metodos para mostras datos en datatable

--- app/s4h/store/ClassifiedConditions/ClassifiedConditionsRepository.php
+++ app/s4h/store/ClassifiedConditions/ClassifiedConditionsRepository.php
@@ -66,5 +66,32 @@ class ClassifiedConditionsRepository extends BaseRepository{
 	{
 		return ClassifiedConditionLang::select()->where('classified_conditions_id','!=',$data['classified_conditions_id'])->where('name','=',$data['name'])->first();
 	}
+
+	public function setDefaultActionColumn() {
+		$this->addColumnToCollection('Actions', function($model)
+		{
+			$language = $this->getCurrentLang();
+
+			$this->cleanActionColumn();
+			
+			$this->addActionColumn("<button href='#fancybox-edit-classified-condition' id='edit_classified-condition_".$model->id."' class='edit-classified-condition btn btn-warning btn-outline dim col-sm-8' style='margin-left: 20px; ' type='button' data-toggle='tooltip' data-placement='top' title='".trans('discountType.actions.Edit')."'  data-original-title='".trans('discountType.actions.Edit')."' ><i class='fa fa-pencil fa-2x'></i>
+					 </button><br/>");
+			$this->addActionColumn("<button href='#' class='delete-classified-condition btn btn-danger btn-outline dim col-sm-8' id='delete_classified-condition_".$model->id."' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('discountType.actions.Delete')."'  data-original-title='".trans('discountType.actions.Delete')."' ><i class='fa fa-times fa-2x'></i>
+					 </button><br/>");
+			$this->addActionColumn("<button href='#fancybox-language-edit-classified-condition' id='language_classified-condition_".$model->id."'  class='edit-classified-condition-lang btn btn-success btn-outline dim col-sm-8' style='margin-left: 20px' type='button' data-toggle='tooltip' data-placement='top' title='".trans('discountType.actions.Language')."'  data-original-title='".trans('discountType.actions.Language')."'> <i class='fa fa-pencil fa-2x'></i></button><br />");
+			return implode(" ", $this->getActionColumn());
+		});
+	}
+
+	public function setBodyTableSettings()
+	{
+		$this->collection->searchColumns('name');
+		$this->collection->orderColumns('name');
+		$this->collection->addColumn('name', function($model)
+		{
+			return $model->InCurrentLang->name;
+		});
+	}
+
 		
 }
