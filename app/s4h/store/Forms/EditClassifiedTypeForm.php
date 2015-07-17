f6272ad624c4ad664e38f2fae74a96db130817a4
ajpadilla <ajpadilla88@gmail.com>
2015-07-16 22:11:39 -0430

    Modificando campos para a validar

--- app/s4h/store/Forms/EditClassifiedTypeForm.php
+++ app/s4h/store/Forms/EditClassifiedTypeForm.php
@@ -8,6 +8,8 @@ use Laracasts\Validation\FormValidator;
 class EditClassifiedTypeForm extends FormValidator {
 
 		protected $rules = [
+			'language_id' =>'required|integer|exists:languages,id',
+			'classified_type_id' =>'required|integer|exists:classified_types,id',
 			'name' => 'required|max:128',
 		];
 	
